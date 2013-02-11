9b9552d1bf3bc87ae7bdb5b94ab150399e3a41bc
ctjoreilly <ctjoreilly@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2013-02-11 18:17:59 +0000

    - adding Distributive Rewriter to Demo.

--- src/main/java/com/sri/ai/grinder/demo/ArithmeticRewritePanel.java
+++ src/main/java/com/sri/ai/grinder/demo/ArithmeticRewritePanel.java
@@ -79,7 +79,8 @@ public class ArithmeticRewritePanel extends AbstractRewritePanel {
 			new ExampleRewrite("Exponentiation: 1^4", "1^4"),
 			// Mixed.
 			new ExampleRewrite("Mixed 1", "2.1/4.2 + 3/4"),
-			new ExampleRewrite("Mixed 2", "1 + 2 - 2 + 5^2 + 49 / 7 + 8 * 4 "),
+			new ExampleRewrite("Mixed 2", "(1 + x + y)*(x + 3)"),
+			new ExampleRewrite("Mixed 3", "1 + 2 - 2 + 5^2 + 49 / 7 + 8 * 4 "),
 		};
 	}
 	
