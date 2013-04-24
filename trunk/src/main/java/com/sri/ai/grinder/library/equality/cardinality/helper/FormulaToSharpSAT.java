fa49bd9c791995d9210d0e7389607086e0f257f6
ctjoreilly <ctjoreilly@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2013-04-24 06:39:31 +0000

    - trivial formula to sharp sat logic in place.

--- trunk/src/main/java/com/sri/ai/grinder/library/equality/cardinality/helper/FormulaToSharpSAT.java
+++ trunk/src/main/java/com/sri/ai/grinder/library/equality/cardinality/helper/FormulaToSharpSAT.java
@@ -37,20 +37,123 @@
  */
 package com.sri.ai.grinder.library.equality.cardinality.helper;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
 import com.google.common.annotations.Beta;
+import com.google.common.base.Predicate;
 import com.sri.ai.expresso.api.Expression;
+import com.sri.ai.expresso.helper.SubExpressionsDepthFirstIterator;
 import com.sri.ai.grinder.api.RewritingProcess;
+import com.sri.ai.grinder.library.Disequality;
+import com.sri.ai.grinder.library.Equality;
+import com.sri.ai.grinder.library.SubExpressionSelection;
+import com.sri.ai.grinder.library.equality.formula.FormulaToCNF;
 
 @Beta
 public class FormulaToSharpSAT {
 	
 	public interface ConversionListener {
 		void start(int numberVariables);
-		void clause(Expression clause);
+		void clause(int[] clause);
 		void end();
 	}
 	
-	public static void converToSharpSAT(Expression formula, RewritingProcess process, ConversionListener conversionListener) {
+	public static void converToSharpSAT(Expression formula, int domainSize, RewritingProcess process, ConversionListener conversionListener) {
+
+		Expression cnfFormula = FormulaToCNF.convertToCNF(formula, process);
+		
+		Map<Expression, Integer> constIds = getConstants(cnfFormula, process);
+		Map<Expression, Integer> varIds   = getVariables(cnfFormula, process);
+		
+		if (constIds.size() > domainSize) {
+			throw new IllegalArgumentException("Domain size too small to represent constants : "+constIds.keySet());
+		}
+		else if (constIds.size() < domainSize) {
+			// Extend with additional constants
+// TODO			
+		}
+		
+		conversionListener.start(varIds.size() * domainSize);
+
 // TODO
+		//
+		// Describe the domain
+		
+		//
+		// Describe the formula
+		for (Expression fClause : cnfFormula.getArguments()) {
+			
+// TODO - do the rewriter to introduce the appropriate formulas for hard literals
+			Expression propFormula = fClause;
+			
+			Expression cnfPropFormula = FormulaToCNF.convertToCNF(propFormula, process);
+			
+			for (Expression pClause : cnfPropFormula.getArguments()) {
+				int[] clause = new int[pClause.numberOfArguments()];
+				int current = 0;
+				for (Expression literal : pClause.getArguments()) {
+					int multiplier = Equality.isEquality(literal) ? 1 : -1;
+					int varId      = varIds.get(literal.get(0));
+					int constId    = constIds.get(literal.get(1));
+					
+					
+					clause[current] = ((varId * domainSize) + constId) * multiplier;
+					current++;
+				}
+				conversionListener.clause(clause);
+			}
+		}
+		
+		conversionListener.end();
+	}
+	
+	//
+	// PRIVATE
+	//
+	private static Map<Expression, Integer> getConstants(Expression formula, final RewritingProcess process) {
+		Set<Expression> consts = new LinkedHashSet<Expression>();
+		
+		Iterator<Expression> subExpressionsIterator =  new SubExpressionsDepthFirstIterator(formula);
+		while (subExpressionsIterator.hasNext()) {
+			Expression expression = subExpressionsIterator.next();
+			if (Equality.isEquality(expression) || Disequality.isDisequality(expression)) {
+				for (Expression term : expression.getArguments()) {
+					if (process.isConstant(term)) {
+						consts.add(term);
+					}
+				}
+			}
+		}
+		
+		Map<Expression, Integer> constIds = new LinkedHashMap<Expression, Integer>();
+		int id = 0;
+		for (Expression cons : consts) {
+			constIds.put(cons, id++);
+		}
+		
+		return constIds;
+	}
+	
+	private static Map<Expression, Integer> getVariables(Expression formula, final RewritingProcess process) {
+		Set<Expression> vars   = new LinkedHashSet<Expression>();
+		
+		vars.addAll(SubExpressionSelection.get(formula, new Predicate<Expression>() {
+			@Override
+			public boolean apply(Expression arg) {
+				return process.isVariable(arg);
+			}
+		}));
+		
+		Map<Expression, Integer> varIds = new LinkedHashMap<Expression, Integer>();
+		int id = 0;
+		for (Expression var : vars) {
+			varIds.put(var, id++);
+		}
+		
+		return varIds;
 	}
 }
