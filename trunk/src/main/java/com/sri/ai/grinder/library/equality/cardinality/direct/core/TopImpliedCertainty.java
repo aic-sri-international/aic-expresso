13687b566cd969389ae2e0c7c64d23d41dbd6fe7
ctjoreilly <ctjoreilly@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2013-04-26 20:50:54 +0000

    - added ability for R_complete_simplify to delegate to an underlying SAT solver for performing tautology and contradiction testing (currently turned off by default).

--- trunk/src/main/java/com/sri/ai/grinder/library/equality/cardinality/direct/core/TopImpliedCertainty.java
+++ trunk/src/main/java/com/sri/ai/grinder/library/equality/cardinality/direct/core/TopImpliedCertainty.java
@@ -44,14 +44,19 @@ import com.google.common.annotations.Beta;
 import com.sri.ai.expresso.api.Expression;
 import com.sri.ai.expresso.api.Symbol;
 import com.sri.ai.expresso.helper.Expressions;
+import com.sri.ai.grinder.GrinderConfiguration;
 import com.sri.ai.grinder.api.RewritingProcess;
 import com.sri.ai.grinder.core.AbstractHierarchicalRewriter;
 import com.sri.ai.grinder.expression.ExpressionCacheKey;
 import com.sri.ai.grinder.helper.Trace;
+import com.sri.ai.grinder.library.boole.And;
 import com.sri.ai.grinder.library.boole.Implication;
+import com.sri.ai.grinder.library.boole.Not;
 import com.sri.ai.grinder.library.equality.cardinality.CardinalityUtil;
 import com.sri.ai.grinder.library.equality.cardinality.direct.CardinalityRewriter;
 import com.sri.ai.grinder.library.equality.formula.FormulaUtil;
+import com.sri.ai.grinder.library.equality.sat.SATSolver;
+import com.sri.ai.grinder.library.equality.sat.SATSolverFactory;
 
 /**
  * Rewriter replacing expression by true or false if it or its negation is implied by the context.
@@ -62,7 +67,9 @@ import com.sri.ai.grinder.library.equality.formula.FormulaUtil;
 @Beta
 public class TopImpliedCertainty extends AbstractHierarchicalRewriter implements CardinalityRewriter {
 	
-	private Set<ExpressionCacheKey> testing = new HashSet<ExpressionCacheKey>();
+	private Set<ExpressionCacheKey> testing      = new HashSet<ExpressionCacheKey>();
+	private boolean                 useSATSolver = GrinderConfiguration.isCompleteSimplifyUseSATSolver();
+	private SATSolver               satSolver    = SATSolverFactory.newInstance();
 	
 	public TopImpliedCertainty() {
 	}
@@ -94,25 +101,57 @@ public class TopImpliedCertainty extends AbstractHierarchicalRewriter implements
 					}
 				}
 				if (test) {
-					// Need to perform the tautology tests with an empty context.
-					RewritingProcess tautologyProcess = process.newSubProcessWithContext(new HashSet<Expression>(), Expressions.TRUE);
+					Expression c         = process.getContextualConstraint();
+					Expression cImpliesF = Implication.make(c, expression);
+					
+					boolean useSAT = useSATSolver && FormulaUtil.isQuantifierFreeFormula(cImpliesF, process);
+					
+					RewritingProcess tautologyProcess = process; 
 					Trace.log("if F is a formula");
-					Expression c            = process.getContextualConstraint();
-					Expression cImpliesF    = Implication.make(c, expression);
-					Expression cImpliesNotF = Implication.make(c, CardinalityUtil.makeNot(expression));
 					Trace.log("    if is_tautology(C => F)");
-					if (IsTautology.isTautology(cImpliesF, tautologyProcess)) {
-						Trace.log("        return true");
-						result = Expressions.TRUE;
-					} 
+					if (useSAT) {
+						// You can use SAT for tautology of a formula F by checking 
+						// if not F is satisfiable. If it is, F is not a tautology.
+						Expression notCImpliesF = Not.make(cImpliesF);
+						if (!satSolver.isSatisfiable(notCImpliesF, process)) {
+							Trace.log("        return true");
+							result = Expressions.TRUE;
+						}
+					}
+					else {
+						// Need to perform the is_tautology tests with an empty context.
+						tautologyProcess = process.newSubProcessWithContext(new HashSet<Expression>(), Expressions.TRUE);
+						if (IsTautology.isTautology(cImpliesF, tautologyProcess)) {
+							Trace.log("        return true");
+							result = Expressions.TRUE;
+						} 
+					}
 					
 					if (result == expression) {
 						Trace.log("    if is_tautology(C => not F)");
-						if (IsTautology.isTautology(cImpliesNotF, tautologyProcess)) {
-							Trace.log("        return false");
-							result = Expressions.FALSE;
+						if (useSAT) {
+							// (c => not F) <=> True (tautology)
+							// <=>
+							// not (c => not F) <=> False
+							// <=>
+							// not (not c or not F) <=> False
+							// <=>
+							// c and F <=> False (unsatisfiable)
+							Expression constraint = And.make(process.getContextualConstraint(), expression);
+							if (!satSolver.isSatisfiable(constraint, process)) {
+								Trace.log("        return false");
+								result = Expressions.FALSE;
+							}
+						}
+						else {
+							Expression cImpliesNotF = Implication.make(c, CardinalityUtil.makeNot(expression));
+							if (IsTautology.isTautology(cImpliesNotF, tautologyProcess)) {
+								Trace.log("        return false");
+								result = Expressions.FALSE;
+							}
 						}
 					}
+					
 					synchronized (testing) {
 						testing.remove(key);
 					}
