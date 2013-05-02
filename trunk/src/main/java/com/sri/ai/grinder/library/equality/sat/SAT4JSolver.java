f55d46841cf8e1e4873d1a1f4023b84e5592cc77
ctjoreilly <ctjoreilly@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2013-05-02 00:30:55 +0000

    - initial code (incomplete) for converting equality logic to propositional form.

--- trunk/src/main/java/com/sri/ai/grinder/library/equality/sat/SAT4JSolver.java
+++ trunk/src/main/java/com/sri/ai/grinder/library/equality/sat/SAT4JSolver.java
@@ -37,6 +37,10 @@
  */
 package com.sri.ai.grinder.library.equality.sat;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
 import org.sat4j.specs.ContradictionException;
@@ -47,13 +51,22 @@ import org.sat4j.specs.TimeoutException;
 import com.google.common.annotations.Beta;
 import com.google.common.base.Throwables;
 import com.sri.ai.expresso.api.Expression;
+import com.sri.ai.expresso.core.DefaultSymbol;
+import com.sri.ai.expresso.helper.Expressions;
 import com.sri.ai.grinder.api.RewritingProcess;
+import com.sri.ai.grinder.library.Disequality;
+import com.sri.ai.grinder.library.Variables;
+import com.sri.ai.grinder.library.boole.And;
 import com.sri.ai.grinder.library.equality.cardinality.helper.FormulaToSharpSAT;
 import com.sri.ai.grinder.library.equality.cardinality.helper.FormulaToSharpSAT.EndState;
+import com.sri.ai.grinder.library.equality.formula.FormulaToNNF;
+import com.sri.ai.grinder.library.equality.formula.FormulaUtil;
 
 @Beta
 public class SAT4JSolver implements SATSolver {
 
+	private boolean useSharpSAT = true;
+	
 	//
 	// START - SATSolver
 	@Override
@@ -62,17 +75,21 @@ public class SAT4JSolver implements SATSolver {
 	}
 	
 	@Override
-	public boolean isSatisfiable(Expression formula, RewritingProcess process) {
+	public boolean isSatisfiable(Expression formula, RewritingProcess process) {		
+		boolean result;
 		
-		SAT4JCall sat4jCall = new SAT4JCall();
-		
+		if (useSharpSAT) {
 // TODO - the SharpSAT conversion is overkill for satisfiablity testing
 // as it generates a propositional grounding intended to be count equivalent
 // as opposed to juse equisatisfiable. Replace with more efficient grounding
 // as detailed in Chapter 4 of Decision Procedures book.
-		FormulaToSharpSAT.convertToSharpSAT(formula, process, sat4jCall);
-		
-		boolean result = sat4jCall.getResult();
+			SAT4JCall sat4jCall = new SAT4JCall();
+			FormulaToSharpSAT.convertToSharpSAT(formula, process, sat4jCall);
+			result = sat4jCall.getResult();
+		}
+		else {
+			result = equalityLogicToPropositionalLogicCall(formula, process);
+		}
 		
 		return result;
 	}
@@ -80,8 +97,88 @@ public class SAT4JSolver implements SATSolver {
 	//
 	
 	//
+	// PRIVATE
 	//
-	//
+	private boolean equalityLogicToPropositionalLogicCall(Expression formula, RewritingProcess process) {
+		boolean result = false;
+		
+		// Remove Constants
+		Expression formulaNoConstants = removeConstants(formula, process);
+		// Convert to NNF
+		Expression formulaNNF         = FormulaToNNF.convertToNNF(formulaNoConstants, process);
+		
+		if (formulaNNF.equals(Expressions.TRUE)) {
+			result = true;
+		}
+		else if (formulaNNF.equals(Expressions.FALSE)) {
+			result = false;
+		}
+		else {	
+// TODO			
+			// Equality Logic to Propositional Logic
+//			Expression propositionalFormula = equalityLogicToPropositional(formulaNNF, process);
+		
+			// Convert to linear CNF and call SAT4J
+//			result                          = convertToLinearCNFAndCallSAT4J(propositionalFormula, process);
+		}
+		
+		return result;
+	}
+	
+	private Expression removeConstants(Expression formula, RewritingProcess process) {
+		Expression result = formula;
+		
+		// Replace each constant in formula with a new variable C_i
+		Set<Expression>  consts       = FormulaUtil.getConstants(formula, process);
+		Set<Expression>  vars         = Variables.get(formula, process);
+		List<Expression> newConstVars = new ArrayList<Expression>(); 		
+		for (Expression c : consts) {
+			Expression newConstVar = newVariable(vars, newConstVars.size()+1);
+			
+			newConstVars.add(newConstVar);
+			
+			result = result.replaceAllOccurrences(c, newConstVar, process);
+		}
+		
+		// For each pair of constants ci, cj such that 1 <= i < j <=n, add the constraint
+		// C_i != C_j to result
+		List<Expression> conjuncts = new ArrayList<Expression>();
+		conjuncts.add(result);
+		for (int i = 0; i < newConstVars.size(); i++) {
+			for (int j = i+1; j < newConstVars.size(); j++) {
+				conjuncts.add(Disequality.make(newConstVars.get(i), newConstVars.get(j)));
+			}
+		}
+		
+		result = And.make(conjuncts);
+		
+		return result;
+	}
+	
+//	private Expression equalityLogicToPropositional(Expression formula, RewritingProcess process) { 
+//		TotalRewriter propositionalRewriter = new TotalRewriter(Arrays.asList((Rewriter)
+//				new BooleanVariableRewriter()
+//			));
+//		Expression result = propositionalRewriter.rewrite(formula, process);	
+//		return result;
+//		
+//		return result;
+//	}
+	
+	private Expression newVariable(Set<Expression> existingVariables, int suffixIdx) {
+		
+		Expression result = null;
+		
+		do {
+			result = DefaultSymbol.createSymbol("C"+suffixIdx);
+			suffixIdx++;
+		} while (existingVariables.contains(result));
+		
+		existingVariables.add(result);
+		
+		return result;
+	}
+	
 	private class SAT4JCall implements FormulaToSharpSAT.ConversionListener {
 		private Boolean result      = null;
 		private ISolver sat4jSolver = null;
