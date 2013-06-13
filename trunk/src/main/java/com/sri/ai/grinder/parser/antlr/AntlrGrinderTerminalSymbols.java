395c94591d19ec11a32572f4b510555043eea92b
ctjoreilly <ctjoreilly@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2013-06-13 18:32:57 +0000

    - added utility class for looking up the terminal symbols in the default Grinder language. - added list of arithmetic functors.

---
+++ trunk/src/main/java/com/sri/ai/grinder/parser/antlr/AntlrGrinderTerminalSymbols.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2013, SRI International
+ * All rights reserved.
+ * Licensed under the The BSD 3-Clause License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://opensource.org/licenses/BSD-3-Clause
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the aic-expresso nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sri.ai.grinder.parser.antlr;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.annotations.Beta;
+
+@Beta
+public class AntlrGrinderTerminalSymbols {
+	// NOTE: ENSURE THIS LIST IS UPDATED WHENEVER THE SYMBOLS IN
+	// AntlrGrinderLexer.g are updated.
+	private static final Set<String> _terminalSymbols;
+	static {
+		Set<String> terminalSymbols = new HashSet<String>();
+		terminalSymbols.add("previous");
+		terminalSymbols.add("message");
+		terminalSymbols.add("lambda");
+		terminalSymbols.add("if");
+		terminalSymbols.add("then");
+		terminalSymbols.add("else");
+		terminalSymbols.add("there");
+		terminalSymbols.add("for");
+		terminalSymbols.add("all");
+		terminalSymbols.add("exists");
+		terminalSymbols.add(":");
+		terminalSymbols.add("<=>");
+		terminalSymbols.add("=>");
+		terminalSymbols.add("->");
+		terminalSymbols.add("!=");
+		terminalSymbols.add(">=");
+		terminalSymbols.add(">");
+		terminalSymbols.add("<=");
+		terminalSymbols.add("<");
+		terminalSymbols.add("=");
+		terminalSymbols.add("+");
+		terminalSymbols.add("-");
+		terminalSymbols.add("minus");
+		terminalSymbols.add("*");
+		terminalSymbols.add("/");
+		terminalSymbols.add("^");
+		terminalSymbols.add("not");
+		terminalSymbols.add("is");
+		terminalSymbols.add("case");
+		terminalSymbols.add("index");
+		terminalSymbols.add("occurs");
+		terminalSymbols.add("from");
+		terminalSymbols.add("variable");
+		terminalSymbols.add("factor");
+		terminalSymbols.add("neighbors");
+		terminalSymbols.add("value");
+		terminalSymbols.add("intersection");
+		terminalSymbols.add("union");
+		terminalSymbols.add("or");
+		terminalSymbols.add("and");
+		terminalSymbols.add("in");
+		terminalSymbols.add("of");
+		terminalSymbols.add("on");
+		terminalSymbols.add("to");
+		terminalSymbols.add("]");
+		terminalSymbols.add("[");
+		terminalSymbols.add("}}");
+		terminalSymbols.add("{{");
+		terminalSymbols.add("_{");
+		terminalSymbols.add("}");
+		terminalSymbols.add("{");
+		terminalSymbols.add("|");
+		terminalSymbols.add("(");
+		terminalSymbols.add(")");
+		terminalSymbols.add(",");
+		terminalSymbols.add("_");
+		terminalSymbols.add(".");
+		
+		_terminalSymbols = Collections.unmodifiableSet(terminalSymbols);
+	}
+
+	public static boolean isTerminalSymbol(String symbol) {
+		boolean result = _terminalSymbols.contains(symbol);
+		return result;
+	}
+}
