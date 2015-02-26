26a865e91ac12bd390ce036401efcb43755ad0fd
rodrigobraz@gmail.com <rodrigobraz@gmail.com@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2015-02-26 01:38:02 +0000

    - implemented scheme for re-using information between different queries in ProbabilisticInference.

--- src/main/java/com/sri/ai/grinder/library/equality/cardinality/plaindpll/ProbabilisticInference.java
+++ src/main/java/com/sri/ai/grinder/library/equality/cardinality/plaindpll/ProbabilisticInference.java
@@ -66,41 +66,89 @@ public class ProbabilisticInference {
 	/**
 	 * Solves a factor graph (Markov network or Bayesian network) provided as a product of potential functions.
 	 * @param factorGraph an Expression representing the product of potential functions
-	 * @param isBayesianNetwork TODO
+	 * @param isBayesianNetwork indicates the factor graph is a bayesian network (each potential function in normalized for one of its variables, forms a DAG).
+	 * @param resultFromPreviousQueryIfKnown the {@link Result} object from a previous query for the same model and evidence, if available, or null.
 	 * @param queryExpression an Expression representing the query
 	 * @param evidence an Expression representing the evidence
-	 * @param mapFromTypeNameToSizeString a map from type name strings to their size strings
-	 * @param mapFromRandomVariableNameToTypeName a map from random variable name strings to their type name strings
+	 * @param mapFromTypeNameToSizeStringParam a map from type name strings to their size strings
+	 * @param mapFromRandomVariableNameToTypeNameParam a map from random variable name strings to their type name strings
 	 * @return
 	 */
 	public static Expression solveFactorGraph(
-			Expression factorGraph, boolean isBayesianNetwork, Expression queryExpression,
-			Expression evidence, Map<String, String> mapFromTypeNameToSizeString, Map<String, String> mapFromRandomVariableNameToTypeName) {
+			Expression factorGraph, boolean isBayesianNetwork, Result resultFromPreviousQueryIfKnown,
+			Expression queryExpression, Expression evidence, Map<String, String> mapFromTypeNameToSizeStringParam, Map<String, String> mapFromRandomVariableNameToTypeNameParam) {
 		
+		Result result = solveFactorGraphAndReturnIntermediateInformation(factorGraph, isBayesianNetwork, resultFromPreviousQueryIfKnown, queryExpression, evidence, mapFromTypeNameToSizeStringParam, mapFromRandomVariableNameToTypeNameParam);
+
+		return result.getQueryMarginal();
+	}
+
+	/**
+	 * same as {@link #solveFactorGraph(Expression, boolean, Result, Expression, Expression, Map, Map),
+	 * but returns an object with all intermediate information that can be reused in a new invocation.
+	 * @param factorGraph
+	 * @param isBayesianNetwork
+	 * @param resultFromPreviousQueryIfKnown
+	 * @param queryExpression
+	 * @param evidence
+	 * @param mapFromTypeNameToSizeStringParam
+	 * @param mapFromRandomVariableNameToTypeNameParam
+	 * @return
+	 */
+	public static Result solveFactorGraphAndReturnIntermediateInformation(Expression factorGraph, boolean isBayesianNetwork, Result resultFromPreviousQueryIfKnown, Expression queryExpression, Expression evidence, Map<String, String> mapFromTypeNameToSizeStringParam, Map<String, String> mapFromRandomVariableNameToTypeNameParam) {
+		Expression evidenceProbability;
+		Map<String, String> mapFromTypeNameToSizeString;
+		Map<String, String> mapFromRandomVariableNameToTypeName;
+		Expression queryVariable;
+		Collection<Expression> allVariables;
+		Predicate<Expression> isUniquelyNamedConstantPredicate;
+		Theory theory;
+		ProblemType problemType;
+		DPLLGeneralizedAndSymbolic solver;
+
+		if (resultFromPreviousQueryIfKnown == null) {
+			mapFromTypeNameToSizeString = mapFromTypeNameToSizeStringParam;
+			mapFromRandomVariableNameToTypeName = mapFromRandomVariableNameToTypeNameParam;
+
+			mapFromRandomVariableNameToTypeName.put("query", "Boolean");
+			queryVariable = parse("query");
+
+			allVariables = Util.mapIntoList(mapFromRandomVariableNameToTypeName.keySet(), Expressions::parse);
+
+			// We use the Prolog convention of small-letter initials for constants, but we need an exception for the random variables.
+			Predicate<Expression> isPrologConstant = new PrologConstantPredicate();
+			isUniquelyNamedConstantPredicate = e -> isPrologConstant.apply(e) && ! allVariables.contains(e);
+
+			// The theory of atoms plus equality on function (relational) terms.
+			theory = new AtomsOnTheoryWithEquality(new EqualityTheory(new SymbolTermTheory()));
+			problemType = new Sum(); // for marginalization
+			// The solver for the parameters above.
+			solver = new DPLLGeneralizedAndSymbolic(theory, problemType);
+
+			evidenceProbability = null;
+		}
+		else {
+			mapFromTypeNameToSizeString = resultFromPreviousQueryIfKnown.getMapFromTypeNameToSizeString();
+			mapFromRandomVariableNameToTypeName = resultFromPreviousQueryIfKnown.getMapFromRandomVariableNameToTypeName();
+			queryVariable = resultFromPreviousQueryIfKnown.getQueryVariable();
+			allVariables = resultFromPreviousQueryIfKnown.getAllVariables();
+			isUniquelyNamedConstantPredicate = resultFromPreviousQueryIfKnown.getIsUniquelyNamedConstantPredicate();
+			theory = resultFromPreviousQueryIfKnown.getTheory();
+			problemType = resultFromPreviousQueryIfKnown.getProblemType();
+			solver = resultFromPreviousQueryIfKnown.getSolver();
+			evidenceProbability = resultFromPreviousQueryIfKnown.getEvidenceProbability();
+		}
+
 		// Add a query variable equivalent to query expression; this introduces no cycles and the model remains a Bayesian network
 		factorGraph = Times.make(list(factorGraph, parse("if query <=> " + queryExpression + " then 1 else 0")));
-		mapFromRandomVariableNameToTypeName.put("query", "Boolean");
-		Expression queryVariable = parse("query");
 		
 		if (evidence != null) {
 			// add evidence factor
 			factorGraph = Times.make(list(factorGraph, IfThenElse.make(evidence, ONE, ZERO)));
 		}
 		
-		Collection<Expression> allVariables = Util.mapIntoList(mapFromRandomVariableNameToTypeName.keySet(), Expressions::parse);
-		
-		// We use the Prolog convention of small-letter initials for constants, but we need an exception for the random variables.
-		Predicate<Expression> isPrologConstant = new PrologConstantPredicate();
-		Predicate<Expression> isUniquelyNamedConstantPredicate = e -> isPrologConstant.apply(e) && ! allVariables.contains(e);
-	
 		// We sum out all variables but the query
 		Collection<Expression> indices = setDifference(allVariables, list(queryVariable)); 
-	
-		// The theory of atoms plus equality on function (relational) terms.
-		Theory theory = new AtomsOnTheoryWithEquality(new EqualityTheory(new SymbolTermTheory()));
-		ProblemType problemType = new Sum(); // for marginalization
-		// The solver for the parameters above.
-		DPLLGeneralizedAndSymbolic solver = new DPLLGeneralizedAndSymbolic(theory, problemType);
 	
 		// Solve the problem.
 		Expression unnormalizedMarginal = solver.solve(factorGraph, indices, mapFromRandomVariableNameToTypeName, mapFromTypeNameToSizeString, isUniquelyNamedConstantPredicate);
@@ -111,18 +159,89 @@ public class ProbabilisticInference {
 		}
 		else {
 			// We now marginalize on all variables. Since unnormalizedMarginal is the marginal on all variables but the query, we simply take that and marginalize on the query alone.
-			Expression normalizationConstant = solver.solve(unnormalizedMarginal, list(queryVariable), mapFromRandomVariableNameToTypeName, mapFromTypeNameToSizeString, isUniquelyNamedConstantPredicate);
-			System.out.println("Normalization constant (same as evidence probability P(" + evidence + ") ) is " + normalizationConstant);
+			if (evidenceProbability == null) {
+				evidenceProbability = solver.solve(unnormalizedMarginal, list(queryVariable), mapFromRandomVariableNameToTypeName, mapFromTypeNameToSizeString, isUniquelyNamedConstantPredicate);
+				System.out.println("Normalization constant (same as evidence probability P(" + evidence + ") ) is " + evidenceProbability);
+			}
 
-			marginal = Division.make(unnormalizedMarginal, normalizationConstant); // Bayes theorem: P(Q | E) = P(Q and E)/P(E)
+			marginal = Division.make(unnormalizedMarginal, evidenceProbability); // Bayes theorem: P(Q | E) = P(Q and E)/P(E)
 			// now we use the algorithm again for simplifying the above division; this is a lazy way of doing this, as it performs search on the query variable again -- we could instead write an ad hoc function to divide all numerical constants by the normalization constant, but the code would be uglier and the gain very small, since this is a search on a single variable anyway.
 			marginal = solver.solve(marginal, list(), mapFromRandomVariableNameToTypeName, mapFromTypeNameToSizeString, isUniquelyNamedConstantPredicate);
 		}
 
 		// replace the query variable with the query expression
 		marginal = marginal.replaceAllOccurrences(queryVariable, queryExpression, new DefaultRewritingProcess(null));
+		
+		// Saves all intermediary information that can be re-used for new queries
+		Result result = new Result(marginal, evidenceProbability, mapFromTypeNameToSizeString, mapFromRandomVariableNameToTypeName, queryVariable, allVariables, isUniquelyNamedConstantPredicate, theory, problemType, solver);
+		return result;
+	}
 
-		return marginal;
+	public static class Result {
+		private Expression queryMarginal;
+		private Expression evidenceProbability;
+		private Map<String, String> mapFromTypeNameToSizeString;
+		private Map<String, String> mapFromRandomVariableNameToTypeName;
+		private Expression queryVariable;
+		private Collection<Expression> allVariables;
+		private Predicate<Expression> isUniquelyNamedConstantPredicate;
+		private Theory theory;
+		private ProblemType problemType;
+		private DPLLGeneralizedAndSymbolic solver;
+	
+		public Result(Expression queryMarginal, Expression evidenceProbability, Map<String, String> mapFromTypeNameToSizeString, Map<String, String> mapFromRandomVariableNameToTypeName, Expression queryVariable, Collection<Expression> allVariables, Predicate<Expression> isUniquelyNamedConstantPredicate, Theory theory, ProblemType problemType, DPLLGeneralizedAndSymbolic solver) {
+			super();
+			this.queryMarginal = queryMarginal;
+			this.evidenceProbability = evidenceProbability;
+			this.mapFromTypeNameToSizeString = mapFromTypeNameToSizeString;
+			this.mapFromRandomVariableNameToTypeName = mapFromRandomVariableNameToTypeName;
+			this.queryVariable = queryVariable;
+			this.allVariables = allVariables;
+			this.isUniquelyNamedConstantPredicate = isUniquelyNamedConstantPredicate;
+			this.theory = theory;
+			this.problemType = problemType;
+			this.solver = solver;
+		}
+
+		public Expression getQueryMarginal() {
+			return queryMarginal;
+		}
+	
+		public Expression getEvidenceProbability() {
+			return evidenceProbability;
+		}
+
+		public Map<String, String> getMapFromTypeNameToSizeString() {
+			return mapFromTypeNameToSizeString;
+		}
+
+		public Map<String, String> getMapFromRandomVariableNameToTypeName() {
+			return mapFromRandomVariableNameToTypeName;
+		}
+
+		public Expression getQueryVariable() {
+			return queryVariable;
+		}
+
+		public Collection<Expression> getAllVariables() {
+			return allVariables;
+		}
+
+		public Predicate<Expression> getIsUniquelyNamedConstantPredicate() {
+			return isUniquelyNamedConstantPredicate;
+		}
+
+		public Theory getTheory() {
+			return theory;
+		}
+
+		public ProblemType getProblemType() {
+			return problemType;
+		}
+
+		public DPLLGeneralizedAndSymbolic getSolver() {
+			return solver;
+		}
 	}
 
 	/**
@@ -162,7 +281,7 @@ public class ProbabilisticInference {
 		Expression queryExpression = parse("earthquake");
 //		Expression queryExpression = parse("earthquake and burglar = bob");
 
-		Expression marginal = solveFactorGraph(bayesianNetwork, true, queryExpression, evidence, mapFromTypeNameToSizeString, mapFromVariableNameToTypeName);
+		Expression marginal = solveFactorGraph(bayesianNetwork, true, null, queryExpression, evidence, mapFromTypeNameToSizeString, mapFromVariableNameToTypeName);
 
 		if (evidence == null) {
 			System.out.println("Query marginal probability P(" + queryExpression + ") is: " + marginal);
