fe0142efe32864be1ef4e709b73fa95eae227332
rodrigobraz@gmail.com <rodrigobraz@gmail.com@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2015-02-26 03:32:27 +0000

    - created AbstractSymbolicGeneralizedSummationSolver.

---
+++ src/main/java/com/sri/ai/grinder/library/equality/cardinality/plaindpll/AbstractSymbolicGeneralizedSummationSolver.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright (c) 2013, SRI International
+ * All rights reserved.
+ * Licensed under the The BSD 3-Clause License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://opensource.org/licenses/BSD-3-Clause
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the aic-expresso nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sri.ai.grinder.library.equality.cardinality.plaindpll;
+
+import static com.sri.ai.expresso.helper.Expressions.FALSE;
+import static com.sri.ai.expresso.helper.Expressions.TRUE;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Predicate;
+import com.sri.ai.expresso.api.Expression;
+import com.sri.ai.expresso.api.IndexExpressionsSet;
+import com.sri.ai.expresso.helper.Expressions;
+import com.sri.ai.grinder.api.Rewriter;
+import com.sri.ai.grinder.api.RewritingProcess;
+import com.sri.ai.grinder.core.AbstractHierarchicalRewriter;
+import com.sri.ai.grinder.core.DefaultRewritingProcess;
+import com.sri.ai.grinder.core.PrologConstantPredicate;
+import com.sri.ai.grinder.helper.GrinderUtil;
+import com.sri.ai.grinder.library.controlflow.IfThenElse;
+import com.sri.ai.grinder.library.equality.cardinality.core.CountsDeclaration;
+import com.sri.ai.grinder.library.equality.cardinality.direct.core.Simplify;
+import com.sri.ai.grinder.library.equality.cardinality.plaindpll.Theory.Constraint;
+import com.sri.ai.grinder.library.indexexpression.IndexExpressions;
+import com.sri.ai.util.Util;
+import com.sri.ai.util.base.Pair;
+
+/**
+ * A "plain" implementation of symbolic generalized DPLL (without using Grinder-style contexts and simplifications)
+ * that is more ad hoc than rewriter-based approaches but also much faster.
+ * 
+ * This generic skeleton allows the algorithm to be used for different tasks.
+ * 
+ * @author braz
+ *
+ */
+abstract public class AbstractSymbolicGeneralizedSummationSolver extends AbstractHierarchicalRewriter implements SymbolicGeneralizedSummationSolver {
+	
+	/** The background theoryWithEquality for the algorithm. */
+	protected Theory theory;
+	
+	/** The problem type being solved. */
+	protected ProblemType problemType;
+
+	/** A {@link CountsDeclaration} encapsulating sort size information. */
+	protected CountsDeclaration countsDeclaration;
+	
+	public AbstractSymbolicGeneralizedSummationSolver(Theory theory, ProblemType problemType) {
+		this(theory, problemType, null);
+	}
+
+	public AbstractSymbolicGeneralizedSummationSolver(Theory theory, ProblemType problemType, CountsDeclaration countsDeclaration) {
+		this.theory = theory;
+		this.problemType = problemType;
+		this.countsDeclaration = countsDeclaration;
+	}
+
+	@Override
+	public Expression rewriteAfterBookkeeping(Expression expression, RewritingProcess process) {
+		Pair<Expression, IndexExpressionsSet> formulaAndIndexExpressions = problemType.getExpressionAndIndexExpressionsFromRewriterProblemArgument(expression, process);
+		Expression formula = formulaAndIndexExpressions.first;
+		IndexExpressionsSet indexExpressions = formulaAndIndexExpressions.second;
+		Expression simplifiedFormula = theory.simplify(formula, process); // eventually this will should not be needed as simplification should be lazy 
+		List<Expression> indices = IndexExpressions.getIndices(indexExpressions);
+		RewritingProcess subProcess = GrinderUtil.extendContextualSymbolsWithIndexExpressions(indexExpressions, process);
+		Expression result = solve(simplifiedFormula, indices, subProcess);
+		return result;
+	}
+
+	@Override
+	public RewritingProcess makeRewritingProcess(Expression expression) {
+		Rewriter rewriterWithModules = new Simplify();
+		RewritingProcess result = new DefaultRewritingProcess(expression, rewriterWithModules);
+		result.notifyReadinessOfRewritingProcess();
+		if (countsDeclaration != null) {
+			countsDeclaration.setup(result);
+		}
+		return result;
+	}
+
+	@Override
+	public Expression solve(
+			Expression expression, Collection<Expression> indices,
+			Map<String, String> mapFromVariableNameToTypeName, Map<String, String> mapFromTypeNameToSizeString) {
+		return solve(expression, indices, mapFromVariableNameToTypeName, mapFromTypeNameToSizeString, new PrologConstantPredicate());
+	}
+	
+	@Override
+	public Expression solve(
+			Expression expression, Collection<Expression> indices,
+			Map<String, String> mapFromVariableNameToTypeName, Map<String, String> mapFromTypeNameToSizeString,
+			Predicate<Expression> isUniquelyNamedConstantPredicate) {
+		
+		RewritingProcess process = new DefaultRewritingProcess(this);
+		for (Map.Entry<String, String> variableNameAndTypeName : mapFromVariableNameToTypeName.entrySet()) {
+			String variableName = variableNameAndTypeName.getKey();
+			String typeName     = variableNameAndTypeName.getValue();
+			process = GrinderUtil.extendContextualSymbolsWithIndexExpression(Expressions.parse(variableName + " in " + typeName), process);
+		}
+		for (Map.Entry<String, String> typeNameAndSizeString : mapFromTypeNameToSizeString.entrySet()) {
+			String typeName   = typeNameAndSizeString.getKey();
+			String sizeString = typeNameAndSizeString.getValue();
+			process.putGlobalObject(Expressions.parse("|" + typeName + "|"), Expressions.parse(sizeString));
+		}
+		
+		process.setIsUniquelyNamedConstantPredicate(isUniquelyNamedConstantPredicate);
+		
+		Expression result = solve(expression, indices, process);
+		return result;
+	}
+
+	/**
+	 * Returns the summation (or the provided semiring additive operation) of an expression over the provided set of indices.
+	 */
+	public Expression solve(Expression expression, Collection<Expression> indices, RewritingProcess process) {
+		// TODO: should replace this oldConstraint by a copy constructor creating a sub-process, but surprisingly there is no complete copy constructor available in DefaultRewritingProcess.
+		Theory.Constraint oldConstraint = process.getDPLLContextualConstraint();
+		Constraint contextualConstraint = theory.makeConstraint(Util.list()); // contextual constraint does not involve any indices -- defined on free variables only
+		process.initializeDPLLContextualConstraint(contextualConstraint);
+
+		Constraint constraint = theory.makeConstraint(indices);
+		Expression result = solve(expression, constraint, process);
+		if (result == null) { // constraint is unsatisfiable, so result is identity element.
+			result = problemType.additiveIdentityElement();
+		}
+		
+		process.initializeDPLLContextualConstraint(oldConstraint);
+		return result;
+	}
+
+	/**
+	 * Returns the summation (or the provided semiring additive operation) of an expression
+	 * over the provided set of indices under given non-null constraint.
+	 */
+	abstract protected Expression solve(Expression expression, Constraint constraint, RewritingProcess process);
+	
+	/**
+	 * Hook method used to normalize unconditional expressions to some normal form chosen by extending classes
+	 * (default is identity).
+	 * @param expression
+	 * @param process
+	 * @return
+	 */
+	public Expression normalizeUnconditionalExpression(Expression expression, RewritingProcess process) {
+		return expression;
+	}
+
+	/**
+	 * If solutions are unconditional expressions, simply add them.
+	 * If they are conditional (symbolic), perform distributive on conditions.
+	 */
+	protected Expression addSymbolicResults(Expression solution1, Expression solution2, RewritingProcess process) {
+
+		Expression result = null;
+
+		if (solution1.equals(problemType.additiveIdentityElement())) {
+			result = theory.applyConstraintToSolution(process.getDPLLContextualConstraint(), solution2, process);
+		}
+		else if (solution2.equals(problemType.additiveIdentityElement())) {
+			result = theory.applyConstraintToSolution(process.getDPLLContextualConstraint(), solution1, process);
+		}
+		else if (DPLLUtil.isConditionalSolution(solution1, theory, process)) {
+			Expression splitter   = IfThenElse.getCondition (solution1);
+			Expression thenBranch = IfThenElse.getThenBranch(solution1);
+			Expression elseBranch = IfThenElse.getElseBranch(solution1);
+
+			Constraint constraint = process.getDPLLContextualConstraint();
+			Expression normalizedSplitter = constraint.normalizeSplitterGivenConstraint(splitter, process);
+
+			if (normalizedSplitter.equals(TRUE)) {
+				result = addSymbolicResults(thenBranch, solution2, process);
+			}
+			else if (normalizedSplitter.equals(FALSE)) {
+				result = addSymbolicResults(elseBranch, solution2, process);
+			}
+			else {
+				RewritingProcess processUnderSplitterAssertion = process.extendDPLLContextualConstraint(true,  normalizedSplitter);
+				RewritingProcess processUnderSplitterNegation  = process.extendDPLLContextualConstraint(false, normalizedSplitter);
+				Expression newThenBranch = addSymbolicResults(thenBranch, solution2, processUnderSplitterAssertion);
+				Expression newElseBranch = addSymbolicResults(elseBranch, solution2, processUnderSplitterNegation);
+				result = IfThenElse.make(normalizedSplitter, newThenBranch, newElseBranch, false /* no simplification to condition */);
+			}
+		}
+		else if (DPLLUtil.isConditionalSolution(solution2, theory, process)) {
+			Expression splitter   = IfThenElse.getCondition (solution2);
+			Expression thenBranch = IfThenElse.getThenBranch(solution2);
+			Expression elseBranch = IfThenElse.getElseBranch(solution2);
+
+			Constraint constraint = process.getDPLLContextualConstraint();
+			Expression normalizedSplitter = constraint.normalizeSplitterGivenConstraint(splitter, process);
+
+			if (normalizedSplitter.equals(TRUE)) {
+				result = addSymbolicResults(solution1, thenBranch, process);
+			}
+			else if (normalizedSplitter.equals(FALSE)) {
+				result = addSymbolicResults(solution1, elseBranch, process);
+			}
+			else {
+				RewritingProcess processUnderSplitterAssertion = process.extendDPLLContextualConstraint(true,  splitter);
+				RewritingProcess processUnderSplitterNegation  = process.extendDPLLContextualConstraint(false, splitter);
+				Expression newThenBranch = addSymbolicResults(solution1, thenBranch, processUnderSplitterAssertion);
+				Expression newElseBranch = addSymbolicResults(solution1, elseBranch, processUnderSplitterNegation);
+				result = IfThenElse.make(normalizedSplitter, newThenBranch, newElseBranch, false /* no simplification to condition */);
+			}
+		}
+		else {
+			result = problemType.add(solution1, solution2, process);
+		}
+	
+		return result;
+	}
+}
