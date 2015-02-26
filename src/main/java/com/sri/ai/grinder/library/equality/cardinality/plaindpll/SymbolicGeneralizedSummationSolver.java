b41bcdf7adf1f403b0331e15a0832e9d35945749
rodrigobraz@gmail.com <rodrigobraz@gmail.com@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2015-02-26 03:17:14 +0000

    - generalized the DPLLGeneralizedAndSymbolic to SymbolicGeneralizedSummationSolver, in preparation for a second implementation of it which uses factorized expressions.

---
+++ src/main/java/com/sri/ai/grinder/library/equality/cardinality/plaindpll/SymbolicGeneralizedSummationSolver.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2013, SRI International
+ * All rights reserved.
+ * Licensed under the The BSD 3-Clause License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://opensource.org/licenses/BSD-3-Clause
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the aic-expresso nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sri.ai.grinder.library.equality.cardinality.plaindpll;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.common.base.Predicate;
+import com.sri.ai.expresso.api.Expression;
+import com.sri.ai.expresso.helper.Expressions;
+import com.sri.ai.grinder.api.Rewriter;
+import com.sri.ai.grinder.api.RewritingProcess;
+import com.sri.ai.grinder.core.DefaultRewritingProcess;
+import com.sri.ai.grinder.core.PrologConstantPredicate;
+import com.sri.ai.grinder.helper.GrinderUtil;
+
+/**
+ * A "plain" implementation of symbolic generalized DPLL (without using Grinder-style contexts and simplifications)
+ * that is more ad hoc than rewriter-based approaches but also much faster.
+ * 
+ * This generic skeleton allows the algorithm to be used for different tasks.
+ * 
+ * @author braz
+ *
+ */
+public interface SymbolicGeneralizedSummationSolver extends Rewriter {
+	
+	/** Solves a problem encoded in the given expression. */
+	Expression rewriteAfterBookkeeping(Expression expression, RewritingProcess process);
+
+	/** Makes a default, appropriate rewriting process for use with this solver as a rewriter. */
+	RewritingProcess makeRewritingProcess(Expression expression);
+
+	/**
+	 * Returns the summation (or the provided semiring additive operation) of an expression over the provided set of indices.
+	 */
+	Expression solve(Expression expression, Collection<Expression> indices, RewritingProcess process);
+
+	/**
+	 * Convenience substitute for {@link #solve(Expression, Collection, RewritingProcess)} that takes care of constructing the RewritingProcess.
+	 */
+	default Expression solve(
+			Expression expression, Collection<Expression> indices,
+			Map<String, String> mapFromVariableNameToTypeName, Map<String, String> mapFromTypeNameToSizeString) {
+		return solve(expression, indices, mapFromVariableNameToTypeName, mapFromTypeNameToSizeString, new PrologConstantPredicate());
+	}
+	
+	/**
+	 * Convenience substitute for {@link #solve(Expression, Collection, RewritingProcess)} that takes care of constructing the RewritingProcess.
+	 */
+	default Expression solve(
+			Expression expression, Collection<Expression> indices,
+			Map<String, String> mapFromVariableNameToTypeName, Map<String, String> mapFromTypeNameToSizeString,
+			Predicate<Expression> isUniquelyNamedConstantPredicate) {
+		
+		RewritingProcess process = new DefaultRewritingProcess(this);
+		for (Map.Entry<String, String> variableNameAndTypeName : mapFromVariableNameToTypeName.entrySet()) {
+			String variableName = variableNameAndTypeName.getKey();
+			String typeName     = variableNameAndTypeName.getValue();
+			process = GrinderUtil.extendContextualSymbolsWithIndexExpression(Expressions.parse(variableName + " in " + typeName), process);
+		}
+		for (Map.Entry<String, String> typeNameAndSizeString : mapFromTypeNameToSizeString.entrySet()) {
+			String typeName   = typeNameAndSizeString.getKey();
+			String sizeString = typeNameAndSizeString.getValue();
+			process.putGlobalObject(Expressions.parse("|" + typeName + "|"), Expressions.parse(sizeString));
+		}
+		
+		process.setIsUniquelyNamedConstantPredicate(isUniquelyNamedConstantPredicate);
+		
+		Expression result = solve(expression, indices, process);
+		return result;
+	}
+}
