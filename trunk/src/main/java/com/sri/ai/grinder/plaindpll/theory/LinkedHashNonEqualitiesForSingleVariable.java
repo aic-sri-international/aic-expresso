1defbc4a768577863c481dfc2b51fee4ed9c5b23
rodrigobraz@gmail.com <rodrigobraz@gmail.com@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2015-03-11 18:05:01 +0000

    - finalized generalization of DisequalitiesConstraintForSingleVariable towards NonEqualityConstraintForSingleVariable and moved those classes out of EqualityTheory.

---
+++ trunk/src/main/java/com/sri/ai/grinder/plaindpll/theory/LinkedHashNonEqualitiesForSingleVariable.java
@@ -0,0 +1,10 @@
+package com.sri.ai.grinder.plaindpll.theory;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+
+import com.sri.ai.expresso.api.Expression;
+
+@SuppressWarnings("serial")
+public class LinkedHashNonEqualitiesForSingleVariable extends LinkedHashMap<String, Collection<Expression>> implements NonEqualitiesForSingleTerm {
+}
