3f55e5b8c429d8bc994d4c235cc685a0bbb585d9
rodrigobraz@gmail.com <rodrigobraz@gmail.com@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2015-03-24 22:13:32 +0000

    - simplified and optimized the updating of representatives in NonEqualitiesConstraint.

--- src/main/java/com/sri/ai/grinder/plaindpll/theory/NonEqualitiesConstraintForSingleVariable.java
+++ src/main/java/com/sri/ai/grinder/plaindpll/theory/NonEqualitiesConstraintForSingleVariable.java
@@ -2,12 +2,9 @@ package com.sri.ai.grinder.plaindpll.theory;
 
 import java.util.List;
 
-import com.google.common.base.Function;
 import com.sri.ai.expresso.api.Expression;
 import com.sri.ai.grinder.api.RewritingProcess;
 import com.sri.ai.grinder.plaindpll.api.Constraint;
-import com.sri.ai.grinder.plaindpll.core.Contradiction;
-import com.sri.ai.util.base.Pair;
 
 /** 
  * Represents a conjunction of literals of binary constraint operators between a variable and
@@ -19,6 +16,9 @@ import com.sri.ai.util.base.Pair;
 public interface NonEqualitiesConstraintForSingleVariable extends Constraint {
 
 	NonEqualitiesConstraintForSingleVariable cloneWithNewNonEqualitiesConstraint(NonEqualitiesConstraint nonEqualitiesConstraint);
+
+	/** Returns the single variable against which all other terms are constrained. */
+	Expression getVariable();
 	
 	/**
 	 * A more efficient replacement for {@link #directlyImpliesLiteral(Expression, RewritingProcess)} for disequality literals.
@@ -35,21 +35,6 @@ public interface NonEqualitiesConstraintForSingleVariable extends Constraint {
 	 * @return
 	 */
 	NonEqualitiesConstraint getNonEqualitiesConstraint(NonEqualitiesConstraint nonEqualitiesConstraint);
-	
-	/**
-	 * Returns a pair of the variable and non-equality constraints map after updating representatives
-	 * (according to parent constraint) in their
-	 * representation, or <code>null</code> if there are no changes.
-	 * The reason this method is needed in this interface and not in Constraint in general is that,
-	 * by representing only the constraints on a single variable,
-	 * their literals may be modified into equivalent ones that cannot be kept within them, but need
-	 * to be moved to some other constraint.
-	 * @param process
-	 * @return
-	 * @throws Contradiction
-	 */
-	Pair<Expression, NonEqualitiesForSingleTerm> updatedTermAndNonEqualitiesPair(
-			Function<Expression, Expression> getRepresentative, RewritingProcess process) throws Contradiction;
 	
 	/**
 	 * Assumes the main variable is a free variable, and returns splitters on it required to hold for this constraint to hold.
