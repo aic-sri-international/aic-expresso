dfaa44058a8f86e2bd0799fc31e7639b1b0db7b2
ctjoreilly <ctjoreilly@3d43a1d2-4736-3da3-5e10-20935d0d7c2c>
2015-04-28 22:04:06 +0000

    Update new functor constant.

--- src/main/java/com/sri/ai/grinder/library/FunctorConstants.java
+++ src/main/java/com/sri/ai/grinder/library/FunctorConstants.java
@@ -119,5 +119,5 @@ public class FunctorConstants {
 	}
 	public static final String CARTESIAN_PRODUCT = "x";
 	public static final String FUNCTION_TYPE = "->";
-	public static final String NUMBER_RANGE = "..";
+	public static final String NUMBER_RANGE = "'..'";
 }
