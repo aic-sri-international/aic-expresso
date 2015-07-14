706a9556882d108ed338d65256a034f490343c3a
Ciaran O'Reilly <ctjoreilly@gmail.com>
2015-07-13 17:39:14 -0700

    Add more Polynomial API tests.

--- src/test/java/com/sri/ai/test/grinder/core/DefaultPolynomialTest.java
+++ src/test/java/com/sri/ai/test/grinder/core/DefaultPolynomialTest.java
@@ -37,12 +37,23 @@
  */
 package com.sri.ai.test.grinder.core;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.sri.ai.expresso.api.Expression;
 import com.sri.ai.expresso.helper.Expressions;
+import com.sri.ai.grinder.api.Monomial;
 import com.sri.ai.grinder.api.Polynomial;
+import com.sri.ai.grinder.core.DefaultMonomial;
 import com.sri.ai.grinder.core.DefaultPolynomial;
+import com.sri.ai.util.math.Rational;
 
 public class DefaultPolynomialTest {
 
@@ -127,6 +138,7 @@ public class DefaultPolynomialTest {
 
 		//
 		// Additions
+		Assert.assertEquals(Expressions.parse("12"), makePolynomial("2 + 10", "tuple(y)"));
 		Assert.assertEquals(Expressions.parse("y + 10"), makePolynomial("y + 10", "tuple(y)"));
 		Assert.assertEquals(Expressions.parse("y + 10"), makePolynomial("y + 10", "(10, y)"));
 		Assert.assertEquals(Expressions.parse("y + 10"), makePolynomial("y + 10", "(y, 10)"));
@@ -139,6 +151,7 @@ public class DefaultPolynomialTest {
 		
 		//
 		// Subtractions
+		Assert.assertEquals(Expressions.parse("-8"), makePolynomial("2 - 10", "tuple(y)"));
 		Assert.assertEquals(Expressions.parse("-1"), makePolynomial("1 - 2", "tuple()"));
 		Assert.assertEquals(Expressions.parse("x"), makePolynomial("2*x - x", "tuple(x)"));
 		Assert.assertEquals(Expressions.parse("0"), makePolynomial("x - x", "tuple(x)"));
@@ -152,71 +165,226 @@ public class DefaultPolynomialTest {
 		Assert.assertEquals(Expressions.parse("-1*y + -10"), makePolynomial("-(y + 10)", "tuple(y)"));
 		Assert.assertEquals(Expressions.parse("x^2*y^4* z + x^2*y^3"), makePolynomial("(y^3 + y^4*z)*x^2", "tuple(x, y, z)"));
 		Assert.assertEquals(Expressions.parse("2*x^2*y + 3*x*y^2 + 4*x^2 + 21*x*y + 15*y^2 + 12*x + 28*y + 5"), makePolynomial("(2*x + 3*y + 5) * (2*x + 5*y + x*y + 1)", "tuple(x, y)"));
+	
+		//
+		// Exponentiation
+// TODO		
+	
+		//
+		// Division
+// TODO		
+	
 	}
 	
 	@Test
 	public void testGetSignatureFactors() {
-		// TODO
+		Polynomial p = makePolynomial("3*x^2*y^4", "tuple()");
+		Assert.assertEquals(Collections.emptyList(), p.getSignatureFactors());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(x)");
+		Assert.assertEquals(Expressions.parse("tuple(x)").getArguments(), p.getSignatureFactors());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(y)");
+		Assert.assertEquals(Expressions.parse("tuple(y)").getArguments(), p.getSignatureFactors());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(3)");
+		Assert.assertEquals(Expressions.parse("tuple(3)").getArguments(), p.getSignatureFactors());
+		
+		p = makePolynomial("3*x^2*y^4", "(x, y)");
+		Assert.assertEquals(Expressions.parse("(x, y)").getArguments(), p.getSignatureFactors());
+		
+		p = makePolynomial("3*x^2*y^4", "(x, y, z)");
+		Assert.assertEquals(Expressions.parse("(x, y, z)").getArguments(), p.getSignatureFactors());
+		
+		p = makePolynomial("3*x^2*y^4", "(x, y, 3)");
+		Assert.assertEquals(Expressions.parse("(x, y, 3)").getArguments(), p.getSignatureFactors());
 	}
 	
 	@Test
 	public void testGetSignatureTermMap() {
-		// TODO
+		Polynomial p = makePolynomial("3*x^2*y^4", "tuple()");
+		Map<List<Rational>, Expression> expectedSignatureTermMap = new HashMap<>();
+		expectedSignatureTermMap.put(Collections.emptyList(), makeMonomial("3*x^2*y^4"));
+		Assert.assertEquals(expectedSignatureTermMap, p.getSignatureTermMap());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(x)");
+		expectedSignatureTermMap = new HashMap<>();
+		expectedSignatureTermMap.put(Arrays.asList(new Rational(2)), Expressions.parse("x^2*(3*y^4)"));
+		Assert.assertEquals(expectedSignatureTermMap, p.getSignatureTermMap());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(y)");
+		expectedSignatureTermMap = new HashMap<>();
+		expectedSignatureTermMap.put(Arrays.asList(new Rational(4)), Expressions.parse("y^4*(3*x^2)"));
+		Assert.assertEquals(expectedSignatureTermMap, p.getSignatureTermMap());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(x, y)");
+		expectedSignatureTermMap = new HashMap<>();
+		expectedSignatureTermMap.put(Arrays.asList(new Rational(2), new Rational(4)), Expressions.parse("3*x^2*y^4"));
+		Assert.assertEquals(expectedSignatureTermMap, p.getSignatureTermMap());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(y, x)");
+		expectedSignatureTermMap = new HashMap<>();
+		expectedSignatureTermMap.put(Arrays.asList(new Rational(4), new Rational(2)), Expressions.parse("3*x^2*y^4"));
+		Assert.assertEquals(expectedSignatureTermMap, p.getSignatureTermMap());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(x, y, z)");
+		expectedSignatureTermMap = new HashMap<>();
+		expectedSignatureTermMap.put(Arrays.asList(new Rational(2), new Rational(4), new Rational(0)), Expressions.parse("3*x^2*y^4"));
+		Assert.assertEquals(expectedSignatureTermMap, p.getSignatureTermMap());
 	}
 	
 	@Test
 	public void testIsMonomial() {
-		// TODO
+		Polynomial p = makePolynomial("0", "(x, y)");
+		Assert.assertTrue(p.isMonomial());
+		
+		p = makePolynomial("1", "(x, y)");
+		Assert.assertTrue(p.isMonomial());
+		
+		// Note: the numeric constants should just be added up together
+		p = makePolynomial("1 + 2 + 4", "(x, y)");
+		Assert.assertTrue(p.isMonomial());
+		
+		p = makePolynomial("3*x^2*y^4", "tuple(y, x)");
+		Assert.assertTrue(p.isMonomial());
+		
+		// Note: the 0 should be dropped, so remains a monomial
+		p = makePolynomial("3*x^2*y^4 + 0", "tuple(y, x)");
+		Assert.assertTrue(p.isMonomial());
+		
+		p = makePolynomial("3*x^2*y^4 + 1", "tuple(y, x)");
+		Assert.assertFalse(p.isMonomial());
 	}
 	
 	@Test
 	public void testAsMonomial() {
-		// TODO
+		Assert.assertEquals(makeMonomial("0"), makePolynomial("0", "(x, y)").asMonomial());
+		Assert.assertEquals(makeMonomial("3*x^2*y^4"), makePolynomial("3*x^2*y^4", "(x, y)").asMonomial());
+	
+		// Note: Due to the signature of factors being a subset they should not equal as variables
+		// in the monomials should be getting treated as constants.
+		Assert.assertNotEquals(makeMonomial("3*x^2*y^4"), makePolynomial("3*x^2*y^4", "tuple(x)").asMonomial());
+		Assert.assertNotEquals(makeMonomial("3*x^2*y^4"), makePolynomial("3*x^2*y^4", "tuple(y)").asMonomial());
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void testAsMonomialIllegalStateException() {
+		makePolynomial("3*x^2*y^4 + x^1", "tuple(y, x)").asMonomial();
 	}
 	
 	@Test
 	public void testNumberOfTerms() {
-		// TODO
+		Assert.assertEquals(1, makePolynomial("0", "(x, y)").numberOfTerms());
+		Assert.assertEquals(1, makePolynomial("3*x^2*y^4", "(x, y)").numberOfTerms());
+		// NOTE: like terms, so are added together automatically
+		Assert.assertEquals(1, makePolynomial("3*x^2*y^4 + x^2*y^4", "(x, y)").numberOfTerms());
+		
+		Assert.assertEquals(2, makePolynomial("3*x^2*y^4 + 3", "(x, y)").numberOfTerms());
+		Assert.assertEquals(3, makePolynomial("3*x^2*y^4 + x*y^3 + 3", "(x, y)").numberOfTerms());
 	}
 	
 	@Test
 	public void testGetOrderedSummands() {
-		// TODO
+		Assert.assertEquals(Arrays.asList(makeMonomial("0")),  makePolynomial("0", "(x, y)").getOrderedSummands());
+		Assert.assertEquals(Arrays.asList(makeMonomial("3*x^2*y^4")),  makePolynomial("3*x^2*y^4", "(x, y)").getOrderedSummands());
+		Assert.assertEquals(Arrays.asList(makeMonomial("4*x^2*y^4")),  makePolynomial("3*x^2*y^4  + x^2*y^4", "(x, y)").getOrderedSummands());
+		
+		Assert.assertEquals(Arrays.asList(makeMonomial("3*x^2*y^4"), makeMonomial("3")),  makePolynomial("3*x^2*y^4  + 3", "(x, y)").getOrderedSummands());
+		Assert.assertEquals(Arrays.asList(makeMonomial("3*x^2*y^4"), makeMonomial("3")),  makePolynomial("3 + 3*x^2*y^4", "(x, y)").getOrderedSummands());
+		
+		Assert.assertEquals(Arrays.asList(makeMonomial("3*x^2*y^4"), makeMonomial("x*y^3"), makeMonomial("3")),  makePolynomial("3*x^2*y^4 + x*y^3 + 3", "(x, y)").getOrderedSummands());
+		Assert.assertEquals(Arrays.asList(makeMonomial("3*x^2*y^4"), makeMonomial("x*y^3"), makeMonomial("3")),  makePolynomial("x*y^3 + 3*x^2*y^4 + 3", "(x, y)").getOrderedSummands());
+		Assert.assertEquals(Arrays.asList(makeMonomial("3*x^2*y^4"), makeMonomial("x*y^3"), makeMonomial("3")),  makePolynomial("x*y^3 + 3 + 3*x^2*y^4", "(x, y)").getOrderedSummands());
 	}
 	
 	@Test
 	public void testIsNumericConstant() {
-		// TODO
+		Assert.assertTrue(makePolynomial("0", "(x, y)").isNumericConstant());
+		Assert.assertTrue(makePolynomial("1", "(x, y)").isNumericConstant());
+		
+		Assert.assertFalse(makePolynomial("x", "(x, y)").isNumericConstant());
+		Assert.assertFalse(makePolynomial("x + 1", "(x, y)").isNumericConstant());
 	}
 	
 	@Test
 	public void testIsZero() {
-		// TODO
+		Assert.assertTrue(makePolynomial("0", "(x, y)").isZero());
+		
+		Assert.assertFalse(makePolynomial("1", "(x, y)").isZero());
+		Assert.assertFalse(makePolynomial("x + 0", "(x, y)").isZero());
 	}
 	
 	@Test
 	public void testIsOne() {
-		// TODO
+		Assert.assertTrue(makePolynomial("1", "(x, y)").isOne());
+		
+		Assert.assertFalse(makePolynomial("0", "(x, y)").isOne());
+		Assert.assertFalse(makePolynomial("x + 1", "(x, y)").isOne());
 	}
 	
 	@Test
 	public void testGetNonNumericConstantFactors() {
-		// TODO
+		Assert.assertEquals(Collections.emptySet(), makePolynomial("0", "(x, y)").getNonNumericConstantFactors());
+		Assert.assertEquals(new HashSet<>(Expressions.parse("(x, y)").getArguments()), makePolynomial("3*x^2*y^4", "(x, y)").getNonNumericConstantFactors());
+		Assert.assertEquals(new HashSet<>(Expressions.parse("(x, 3*y^4)").getArguments()), makePolynomial("3*x^2*y^4", "tuple(x)").getNonNumericConstantFactors());
+		Assert.assertEquals(new HashSet<>(Expressions.parse("(y, 3*x^2)").getArguments()), makePolynomial("3*x^2*y^4", "tuple(y)").getNonNumericConstantFactors());
+		Assert.assertEquals(new HashSet<>(Expressions.parse("tuple(x^2*y^4)").getArguments()), makePolynomial("3*x^2*y^4", "tuple(3)").getNonNumericConstantFactors());
+		Assert.assertEquals(new HashSet<>(Expressions.parse("(x, y)").getArguments()), makePolynomial("3*x^2*y^4", "(x, y, 3)").getNonNumericConstantFactors());
+		Assert.assertEquals(new HashSet<>(Expressions.parse("(x, y, z)").getArguments()), makePolynomial("3*x^2*y^4 + z + 10", "(x, y, z)").getNonNumericConstantFactors());
+		Assert.assertEquals(new HashSet<>(Expressions.parse("(x, 3*y^4, 10*z)").getArguments()), makePolynomial("3*x^2*y^4 + 10*z", "tuple(x)").getNonNumericConstantFactors());
+
 	}
 	
 	@Test
 	public void testAdd() {
-		// TODO
+		Assert.assertEquals(Expressions.parse("12"), makePolynomial("2", "tuple(y)").add(makePolynomial("10", "tuple(y)")));
+		Assert.assertEquals(Expressions.parse("y + 10"), makePolynomial("y", "tuple(y)").add(makePolynomial("10", "tuple(y)")));
+		Assert.assertEquals(Expressions.parse("y + 10"), makePolynomial("y", "tuple(10, y)").add(makePolynomial("10", "tuple(10, y)")));
+		Assert.assertEquals(Expressions.parse("y + 10"), makePolynomial("y", "tuple(y, 10)").add(makePolynomial("10", "tuple(y, 10)")));
+		Assert.assertEquals(Expressions.parse("y + 10"), makePolynomial("y", "tuple()").add(makePolynomial("10", "tuple()")));
+	
+		Assert.assertEquals(Expressions.parse("2*y + 10"), makePolynomial("2*y", "tuple(y)").add(makePolynomial("10", "tuple(y)")));
+		
+
+		Assert.assertEquals(Expressions.parse("x^3*y^2 + 2*x^2 + y^2 + x + y + 10"), 
+				makePolynomial("x^2", "(x, y)")
+					.add(makePolynomial("7", "(x, y)"))
+					.add(makePolynomial("x^3*y^2", "(x, y)"))
+					.add(makePolynomial("3", "(x, y)"))
+					.add(makePolynomial("y^2", "(x, y)"))
+					.add(makePolynomial("x^2", "(x, y)"))
+					.add(makePolynomial("x", "(x, y)"))
+					.add(makePolynomial("y", "(x, y)"))
+		);
+		Assert.assertEquals(Expressions.parse("5*x*y + 4*y^2 + x + 6"), 
+				makePolynomial("(3*x^2 - 2*x + 5*x*y - 2)", "(x, y)")
+					.add(makePolynomial("-3*x^2 + 3*x + 4*y^2 + 8", "(x, y)"))
+		);
 	}
 	
 	@Test
 	public void testMinus() {
-		// TODO
+		Assert.assertEquals(Expressions.parse("-8"), makePolynomial("2", "tuple(y)").minus(makePolynomial("10", "tuple(y)")));
+		Assert.assertEquals(Expressions.parse("-1"), makePolynomial("1", "tuple()").minus(makePolynomial("2", "tuple()")));
+		Assert.assertEquals(Expressions.parse("x"), makePolynomial("2*x", "tuple(x)").minus(makePolynomial("x", "tuple(x)")));
+		Assert.assertEquals(Expressions.parse("0"), makePolynomial("x", "tuple(x)").minus(makePolynomial("x", "tuple(x)")));
+		Assert.assertEquals(Expressions.parse("0"), makePolynomial("2*x", "tuple(x)").minus(makePolynomial("x", "tuple(x)"))
+				.minus(makePolynomial("x", "tuple(x)")));
+		Assert.assertEquals(Expressions.parse("x"), makePolynomial("x", "tuple(x)").minus(makePolynomial("0", "tuple(x)")));
+		Assert.assertEquals(Expressions.parse("-1*x"), makePolynomial("0", "tuple(x)").minus(makePolynomial("x", "tuple(x)")));
+		Assert.assertEquals(Expressions.parse("2*x^3 + -1*x^2 + -1*x"), makePolynomial("2*x^3", "tuple(x)")
+				.minus(makePolynomial("x^2", "tuple(x)"))
+				.minus(makePolynomial("x", "tuple(x)")));
 	}
 	
 	@Test
 	public void testTimes() {
-		// TODO
+		Assert.assertEquals(Expressions.parse("-1*y + -10"), makePolynomial("-1", "tuple(y)")
+				.times(makePolynomial("y + 10", "tuple(y)")));
+		Assert.assertEquals(Expressions.parse("x^2*y^4* z + x^2*y^3"), makePolynomial("y^3 + y^4*z", "tuple(x, y, z)")
+				.times(makePolynomial("x^2", "tuple(x, y, z)")));
+		Assert.assertEquals(Expressions.parse("2*x^2*y + 3*x*y^2 + 4*x^2 + 21*x*y + 15*y^2 + 12*x + 28*y + 5"), makePolynomial("2*x + 3*y + 5", "tuple(x, y)")
+				.times(makePolynomial("2*x + 5*y + x*y + 1", "tuple(x, y)")));
 	}
 	
 	@Test
@@ -238,17 +406,40 @@ public class DefaultPolynomialTest {
 	// FunctionaApplication API related tests
 	@Test
 	public void testGetFunctor() {
-		// TODO
+		Polynomial p = makePolynomial("0", "(x, y)");
+		Assert.assertNull(p.getFunctor());
+		
+		p = makePolynomial("z^4*y^2*x^3", "(x, y)");
+		Assert.assertEquals(Expressions.makeSymbol("*"), p.getFunctor());
+		
+		p = makePolynomial("z^4*y^2*x^3 + 10", "(x, y)");
+		Assert.assertEquals(Expressions.makeSymbol("+"), p.getFunctor());
 	}
 	
 	@Test
 	public void testGetArguments() {
-		// TODO
+		List<Expression> args = makePolynomial("0", "(x, y)").getArguments();
+		Assert.assertEquals(0, args.size());
+		
+		args = makePolynomial("x^2*y^3", "(x, y)").getArguments();
+		Assert.assertEquals(2, args.size());
+		Assert.assertEquals(Expressions.parse("(x^2, y^3)").getArguments(), args);
+		
+		args = makePolynomial("x^2*y^3 + 10", "(x, y)").getArguments();
+		Assert.assertEquals(2, args.size());
+		Assert.assertEquals(Expressions.parse("(x^2*y^3, 10)").getArguments(), args);
 	}
 	
 	@Test
 	public void testNumberOfArguments() {
-		// TODO
+		Polynomial p = makePolynomial("0", "(x, y)");
+		Assert.assertEquals(0, p.numberOfArguments());
+		
+		p = makePolynomial("x^2*y^3", "(x, y)");
+		Assert.assertEquals(2, p.numberOfArguments());
+		
+		p = makePolynomial("x^2*y^3 + 10", "(x, y)");
+		Assert.assertEquals(2, p.numberOfArguments());
 	}
 	
 	@Test
@@ -281,6 +472,11 @@ public class DefaultPolynomialTest {
 	//
 	// PRIVATE
 	//	
+	private static Monomial makeMonomial(String monomial) {
+		Monomial result = DefaultMonomial.make(Expressions.parse(monomial));
+		return result;
+	}
+	
 	private static Polynomial makePolynomial(String polynomial, String tupleSignatureFactors) {
 		Polynomial result = DefaultPolynomial.make(Expressions.parse(polynomial), Expressions.parse(tupleSignatureFactors).getArguments());
 		return result;
