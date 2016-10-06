74779589fb8819e0c6e349f2353132bb907e9d8b
Ciaran O'Reilly <ctjoreilly@gmail.com>
2016-10-05 21:23:46 -0700

    Add tests for GrinderUtil.isTypeSubtypeOf()

--- src/test/java/com/sri/ai/test/grinder/helper/GrinderUtilTest.java
+++ src/test/java/com/sri/ai/test/grinder/helper/GrinderUtilTest.java
@@ -37,12 +37,255 @@
  */
 package com.sri.ai.test.grinder.helper;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class GrinderUtilTest {
+import static com.sri.ai.expresso.helper.Expressions.parse;
+import static com.sri.ai.grinder.helper.GrinderUtil.BOOLEAN_TYPE;
+import static com.sri.ai.grinder.helper.GrinderUtil.INTEGER_TYPE;
+import static com.sri.ai.grinder.helper.GrinderUtil.REAL_TYPE;
+import static com.sri.ai.grinder.helper.GrinderUtil.isTypeSubtypeOf;
 
+import com.sri.ai.expresso.type.Categorical;
+import com.sri.ai.expresso.type.FunctionType;
+import com.sri.ai.expresso.type.IntegerInterval;
+import com.sri.ai.expresso.type.RealInterval;
+
+public class GrinderUtilTest {
+	
 	@Test
-	public void testIsTypeSubtypeOf() {
-// TODO		
+	public void testIsBooleanTypeSubtypeOf() {
+		Assert.assertTrue(isTypeSubtypeOf(BOOLEAN_TYPE, BOOLEAN_TYPE));		
+		Assert.assertFalse(isTypeSubtypeOf(BOOLEAN_TYPE, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(BOOLEAN_TYPE, REAL_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(BOOLEAN_TYPE, new IntegerInterval("Integer")));
+		Assert.assertFalse(isTypeSubtypeOf(BOOLEAN_TYPE, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(BOOLEAN_TYPE, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(BOOLEAN_TYPE, new RealInterval("[-0.5;4.5]")));
+		Assert.assertFalse(isTypeSubtypeOf(BOOLEAN_TYPE, new FunctionType(BOOLEAN_TYPE)));
+		Assert.assertFalse(isTypeSubtypeOf(BOOLEAN_TYPE, new FunctionType(BOOLEAN_TYPE, BOOLEAN_TYPE)));
+	}
+	
+	@Test
+	public void testIsIntegerTypeSubtypeOf() {
+		Assert.assertFalse(isTypeSubtypeOf(INTEGER_TYPE, BOOLEAN_TYPE));		
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, INTEGER_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, new IntegerInterval("Integer")));
+		Assert.assertFalse(isTypeSubtypeOf(INTEGER_TYPE, new IntegerInterval("0..4")));
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, new IntegerInterval("-infinity..infinity")));
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, new RealInterval("Real")));
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, new RealInterval("[-infinity;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, new RealInterval("]-infinity;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, new RealInterval("[-infinity;infinity[")));
+		Assert.assertTrue(isTypeSubtypeOf(INTEGER_TYPE, new RealInterval("]-infinity;infinity[")));
+		Assert.assertFalse(isTypeSubtypeOf(INTEGER_TYPE, new RealInterval("[-0.5;4.5]")));
+		Assert.assertFalse(isTypeSubtypeOf(INTEGER_TYPE, new FunctionType(INTEGER_TYPE)));
+		Assert.assertFalse(isTypeSubtypeOf(INTEGER_TYPE, new FunctionType(INTEGER_TYPE, INTEGER_TYPE)));
+	}
+	
+	@Test
+	public void testIsRealTypeSubtypeOf() {
+		Assert.assertFalse(isTypeSubtypeOf(REAL_TYPE, BOOLEAN_TYPE));		
+		Assert.assertFalse(isTypeSubtypeOf(REAL_TYPE, INTEGER_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(REAL_TYPE, REAL_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(REAL_TYPE, new IntegerInterval("Integer")));
+		Assert.assertFalse(isTypeSubtypeOf(REAL_TYPE, new IntegerInterval("0..4")));
+		Assert.assertTrue(isTypeSubtypeOf(REAL_TYPE, new RealInterval("Real")));
+		Assert.assertTrue(isTypeSubtypeOf(REAL_TYPE, new RealInterval("[-infinity;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(REAL_TYPE, new RealInterval("]-infinity;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(REAL_TYPE, new RealInterval("[-infinity;infinity[")));
+		Assert.assertTrue(isTypeSubtypeOf(REAL_TYPE, new RealInterval("]-infinity;infinity[")));
+		Assert.assertFalse(isTypeSubtypeOf(REAL_TYPE, new RealInterval("[-0.5;4.5]")));
+		Assert.assertFalse(isTypeSubtypeOf(REAL_TYPE, new FunctionType(REAL_TYPE)));
+		Assert.assertFalse(isTypeSubtypeOf(REAL_TYPE, new FunctionType(REAL_TYPE, REAL_TYPE)));
+	}
+	
+	@Test
+	public void testIsCategoricalTypeSubtypeOf() {
+		Categorical categoricalType = new Categorical("TestCategorical1", 10, parse("a"), parse("b"), parse("c"));
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, BOOLEAN_TYPE));		
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, REAL_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, new IntegerInterval("Integer")));
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, new RealInterval("[-0.5;4.5]")));
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, new FunctionType(categoricalType)));
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType, new FunctionType(categoricalType, categoricalType)));
+		
+		// NOTE: Categorical types equality is based on name.
+		Assert.assertTrue(isTypeSubtypeOf(categoricalType,  new Categorical("TestCategorical1", 10, parse("a"), parse("b"), parse("c"))));
+		Assert.assertTrue(isTypeSubtypeOf(categoricalType,  new Categorical("TestCategorical1", 5, parse("x"), parse("y"), parse("z"))));
+		
+		Assert.assertFalse(isTypeSubtypeOf(categoricalType,  new Categorical("TestCategorical2", 10, parse("a"), parse("b"), parse("c"))));
+	}
+	
+	@Test
+	public void testIsIntegerIntervalSubtypeOf() {
+		IntegerInterval intInterval = new IntegerInterval("Integer");
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, BOOLEAN_TYPE));		
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, INTEGER_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new IntegerInterval("Integer")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("0..4")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[-0.5;4.5]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new FunctionType(intInterval)));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new FunctionType(intInterval, intInterval)));
+		
+		intInterval = new IntegerInterval("0..4");
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, INTEGER_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("1..4")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("0..3")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("Real")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("[0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("]0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;4[")));
+		
+		intInterval = new IntegerInterval("-infinity..4");
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("1..4")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("0..3")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new IntegerInterval("-infinity..5")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("]0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;4[")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("[-infinity;4]")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("]-infinity;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[-infinity;4[")));
+		
+		intInterval = new IntegerInterval("0..infinity");
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("1..4")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("0..3")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new IntegerInterval("-1..infinity")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("]0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;4[")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("[0;infinity]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("]0;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("[0;infinity[")));
+		
+		intInterval = new IntegerInterval("-infinity..infinity");
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("1..4")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new IntegerInterval("0..3")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("]0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;4[")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[-infinity;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("]-infinity;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[-infinity;4[")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;infinity]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("]0;infinity]")));
+		Assert.assertFalse(isTypeSubtypeOf(intInterval, new RealInterval("[0;infinity[")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("[-infinity;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("]-infinity;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("[-infinity;infinity[")));
+		Assert.assertTrue(isTypeSubtypeOf(intInterval, new RealInterval("]-infinity;infinity[")));
+	}
+	
+	@Test
+	public void testIsRealIntervalSubtypeOf() {
+		RealInterval realInterval = new RealInterval("Real");
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, BOOLEAN_TYPE));		
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, INTEGER_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, REAL_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("Integer")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..4")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[-0.5;4.5]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new FunctionType(realInterval)));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new FunctionType(realInterval, realInterval)));
+		
+		realInterval = new RealInterval("[0;4]");
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("1..4")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..3")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("Real")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("[0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("]0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;4[")));
+		
+		realInterval = new RealInterval("[-infinity;4]");
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("1..4")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..3")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("-infinity..5")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("]0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;4[")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("[-infinity;4]")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("]-infinity;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[-infinity;4[")));
+		
+		realInterval = new RealInterval("[0;infinity]");
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("1..4")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..3")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("-1..infinity")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("]0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;4[")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("[0;infinity]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("]0;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("[0;infinity[")));
+		
+		realInterval = new RealInterval("[-infinity;infinity]");
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, INTEGER_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..4")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("1..4")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new IntegerInterval("0..3")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, REAL_TYPE));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("Real")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("]0;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;4[")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[-infinity;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("]-infinity;4]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[-infinity;4[")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;infinity]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("]0;infinity]")));
+		Assert.assertFalse(isTypeSubtypeOf(realInterval, new RealInterval("[0;infinity[")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("[-infinity;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("]-infinity;infinity]")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("[-infinity;infinity[")));
+		Assert.assertTrue(isTypeSubtypeOf(realInterval, new RealInterval("]-infinity;infinity[")));
+	}
+	
+	@Test
+	public void testIsFunctionTypeSubtypeOf() {
+		Assert.assertTrue(isTypeSubtypeOf(new FunctionType(BOOLEAN_TYPE), new FunctionType(BOOLEAN_TYPE)));
+		Assert.assertTrue(isTypeSubtypeOf(new FunctionType(BOOLEAN_TYPE, BOOLEAN_TYPE), new FunctionType(BOOLEAN_TYPE, BOOLEAN_TYPE)));
+
+		Assert.assertFalse(isTypeSubtypeOf(new FunctionType(BOOLEAN_TYPE, BOOLEAN_TYPE), BOOLEAN_TYPE));
+		Assert.assertFalse(isTypeSubtypeOf(new FunctionType(BOOLEAN_TYPE), new FunctionType(BOOLEAN_TYPE, BOOLEAN_TYPE)));
+		Assert.assertFalse(isTypeSubtypeOf(new FunctionType(BOOLEAN_TYPE, BOOLEAN_TYPE), new FunctionType(BOOLEAN_TYPE)));
+
+		Assert.assertTrue(isTypeSubtypeOf(new FunctionType(INTEGER_TYPE), new FunctionType(REAL_TYPE)));
+		Assert.assertFalse(isTypeSubtypeOf(new FunctionType(REAL_TYPE), new FunctionType(INTEGER_TYPE)));
+				
+		Assert.assertTrue(isTypeSubtypeOf(new FunctionType(INTEGER_TYPE, REAL_TYPE), new FunctionType(REAL_TYPE, INTEGER_TYPE)));
+		Assert.assertFalse(isTypeSubtypeOf(new FunctionType(INTEGER_TYPE, INTEGER_TYPE), new FunctionType(REAL_TYPE, REAL_TYPE)));
 	}
 }
