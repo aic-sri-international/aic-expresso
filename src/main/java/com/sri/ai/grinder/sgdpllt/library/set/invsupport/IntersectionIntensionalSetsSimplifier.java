0b715223ea2df11b4ec9f6684fd97a0e36addfa1
Ciaran O'Reilly <oreilly@ai.sri.com>
2017-01-23 17:28:10 -0800

    Add initial implementation of IntersectionIntensionalSetsSimplifier.

---
+++ src/main/java/com/sri/ai/grinder/sgdpllt/library/set/invsupport/IntersectionIntensionalSetsSimplifier.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2017, SRI International
+ * All rights reserved.
+ * Licensed under the The BSD 3-Clause License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://opensource.org/licenses/BSD-3-Clause
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the aic-expresso nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sri.ai.grinder.sgdpllt.library.set.invsupport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sri.ai.expresso.api.Expression;
+import com.sri.ai.expresso.api.IndexExpressionsSet;
+import com.sri.ai.expresso.api.IntensionalSet;
+import com.sri.ai.grinder.helper.GrinderUtil;
+import com.sri.ai.grinder.sgdpllt.api.Context;
+import com.sri.ai.grinder.sgdpllt.library.Equality;
+import com.sri.ai.grinder.sgdpllt.library.FunctorConstants;
+import com.sri.ai.grinder.sgdpllt.library.boole.And;
+import com.sri.ai.grinder.sgdpllt.library.boole.ThereExists;
+import com.sri.ai.grinder.sgdpllt.library.set.Sets;
+import com.sri.ai.grinder.sgdpllt.rewriter.api.Simplifier;
+
+/**
+ * <pre>
+ * {{ (on I1) H1 : C1 }} intersection {{ (on I2) H2 : C2 }}  
+ * ---> 
+ * {{ (on I1) H1 : C1 and evaluate(there exists I2 : C2 and H2 = H1) }} 
+ * </pre>
+ *  
+ * @author oreilly
+ *
+ */
+public class IntersectionIntensionalSetsSimplifier implements Simplifier {
+	@Override
+	public Expression applySimplifier(Expression expression, Context context) {
+		return simplify(expression, context);
+	}
+
+	public static Expression simplify(Expression expression, Context context) {
+		Expression result = expression;
+		
+		if (expression.hasFunctor(FunctorConstants.INTERSECTION)) {
+			List<Expression> intensionalMultiSetArgs    = new ArrayList<>();
+			List<Expression> nonIntensionalMultiSetArgs = new ArrayList<>();
+			for (Expression arg : expression.getArguments()) {
+				if (Sets.isIntensionalMultiSet(arg)) {
+					intensionalMultiSetArgs.add(arg);
+				}
+				else {
+					nonIntensionalMultiSetArgs.add(arg);
+				}
+			}
+			if (intensionalMultiSetArgs.size() > 1) {
+				boolean resultIsEmptySet = false;
+				IntensionalSet intersectedMultiSet = (IntensionalSet) intensionalMultiSetArgs.get(0);
+				for (int i = 1; i < intensionalMultiSetArgs.size() && !resultIsEmptySet; i++) {
+					IntensionalSet otherMultiSet = (IntensionalSet) intensionalMultiSetArgs.get(i);
+					// {{ (on I1) H1 : C1 }} intersection {{ (on I2) H2 : C2 }}
+					// ---->
+					// {{ (on I1) H1 : C1 and evaluate(there exists I2 : C2 and H2 = H1) }}
+// TODO - need to handle name clashes across intensional sets, i.e. standardize apart.					
+					IndexExpressionsSet i1 = intersectedMultiSet.getIndexExpressions();
+					IndexExpressionsSet i2 = otherMultiSet.getIndexExpressions();
+					Expression h1 = intersectedMultiSet.getHead();
+					Expression h2 = otherMultiSet.getHead();
+					Expression c1 = intersectedMultiSet.getCondition();
+					Expression c2 = otherMultiSet.getCondition();
+					
+					Expression thereExists          = ThereExists.make(i2, And.make(c2, Equality.make(h2, h1)));
+					Context    thereExistsContext   = (Context) GrinderUtil.extendRegistryWithIndexExpressions(i1, context);
+					Expression thereExistsEvaluated = context.getTheory().evaluate(thereExists, thereExistsContext);
+					if (thereExistsEvaluated.equals(false)) {
+						// They don't intersect, which means you have an empty
+						// set in the intersection, which means the whole thing
+						// results in the empty set.
+						resultIsEmptySet = true;
+					}
+					else if (!thereExistsEvaluated.equals(true)) {
+						// If we have a condition, other than false and true
+						// we will want to extend the current result by the condition
+						Expression extendedCondition = And.make(c1, thereExistsEvaluated);
+						intersectedMultiSet = (IntensionalSet) IntensionalSet.intensionalMultiSet(i1, h1, extendedCondition);
+					}
+				}
+				if (resultIsEmptySet) {
+					result = Sets.EMPTY_SET;
+				}
+				else if (nonIntensionalMultiSetArgs.size() > 0) {
+					List<Expression> intersectedArgs = new ArrayList<>();
+					intersectedArgs.add(intersectedMultiSet);
+					intersectedArgs.addAll(nonIntensionalMultiSetArgs);
+					result = Sets.makeIntersection(intersectedArgs.toArray(new Expression[intersectedArgs.size()]));
+				}
+				else {
+					result = intersectedMultiSet;
+				}
+			}
+		}
+		
+		return result;
+	}
+}
---
+++ src/test/java/com/sri/ai/test/grinder/sgdpllt/library/set/invsupport/IntersectionIntensionalSetsSimplifierTest.java
@@ -0,0 +1,57 @@
+package com.sri.ai.test.grinder.sgdpllt.library.set.invsupport;
+
+import static com.sri.ai.expresso.helper.Expressions.parse;
+import static com.sri.ai.util.Util.map;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sri.ai.expresso.api.Expression;
+import com.sri.ai.expresso.type.IntegerInterval;
+import com.sri.ai.grinder.helper.GrinderUtil;
+import com.sri.ai.grinder.sgdpllt.api.Context;
+import com.sri.ai.grinder.sgdpllt.core.TrueContext;
+import com.sri.ai.grinder.sgdpllt.library.set.invsupport.IntersectionIntensionalSetsSimplifier;
+import com.sri.ai.grinder.sgdpllt.theory.compound.CompoundTheory;
+import com.sri.ai.grinder.sgdpllt.theory.differencearithmetic.DifferenceArithmeticTheory;
+import com.sri.ai.grinder.sgdpllt.theory.tuple.TupleTheory;
+
+public class IntersectionIntensionalSetsSimplifierTest {
+	private Context context;
+	private IntersectionIntensionalSetsSimplifier simplifier;
+	
+	@Before
+	public void setUp() {
+		context = new TrueContext(
+				new CompoundTheory(
+						new DifferenceArithmeticTheory(false, false), 
+						new TupleTheory()));
+		IntegerInterval nType = new IntegerInterval(1, 10);
+		context = (Context) GrinderUtil.extendRegistryWith(
+				map("N", nType.toString()), 
+				Arrays.asList(nType), context);
+		
+		simplifier = new IntersectionIntensionalSetsSimplifier();
+	}
+	
+	@Test
+	public void testBasicCases() {
+		Expression intersection = parse("{{(on I in 1..10) (I, 2) : I != 5}} intersection {{(on J in 1..10) (J, 2) : J != 4}}");
+		Assert.assertEquals(
+				parse("{{(on I in 1..10) (I, 2) : I != 5 and I != 4}}"),
+				simplifier.apply(intersection, context));
+		
+		intersection = parse("{{(on I in 1..10) (I, 2) : I != 5}} intersection {{(on J in 1..10) (J, 3) : J != 4}}");
+		Assert.assertEquals(
+				parse("{}"),
+				simplifier.apply(intersection, context));
+		
+		intersection = parse("{{(on I in 1..10) (I, 2) : I != 5}} intersection {{(on J in 1..10) (J, 2) : J != 4}} intersection {{(10, 2)}}");
+		Assert.assertEquals(
+				parse("{{(on I in 1..10) (I, 2) : I != 5 and I != 4}} intersection {{(10,2)}}"),
+				simplifier.apply(intersection, context));
+	}
+}
