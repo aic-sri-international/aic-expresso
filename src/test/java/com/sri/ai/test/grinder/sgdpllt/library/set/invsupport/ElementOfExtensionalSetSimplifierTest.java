feeb4f1e51ce75becd0c74ccbb3356e2c85e041a
Ciaran O'Reilly <oreilly@ai.sri.com>
2017-04-11 18:52:05 -0700

    Add element of extensional set simplifier.
    Along with supporting tests.

---
+++ src/test/java/com/sri/ai/test/grinder/sgdpllt/library/set/invsupport/ElementOfExtensionalSetSimplifierTest.java
@@ -0,0 +1,73 @@
+package com.sri.ai.test.grinder.sgdpllt.library.set.invsupport;
+
+import org.junit.Test;
+
+import com.sri.ai.grinder.sgdpllt.api.Context;
+import com.sri.ai.grinder.sgdpllt.core.TrueContext;
+import com.sri.ai.grinder.sgdpllt.library.set.invsupport.ElementOfExtensionalSetSimplifier;
+
+import org.junit.Assert;
+
+import static com.sri.ai.expresso.helper.Expressions.parse;
+
+public class ElementOfExtensionalSetSimplifierTest {
+	private ElementOfExtensionalSetSimplifier simplifier = new ElementOfExtensionalSetSimplifier();
+	private Context context = new TrueContext();
+	
+	@Test
+	public void testElementInEmptySet() {
+		Assert.assertEquals(
+				parse("false"), 
+				simplifier.apply(parse("A in {}"), context));
+		
+		Assert.assertEquals(
+				parse("false"), 
+				simplifier.apply(parse("A in {{}}"), context));
+	}
+	
+	@Test
+	public void testElementInSingletonSet() {
+		Assert.assertEquals(
+				parse("A = B"), 
+				simplifier.apply(parse("A in {B}"), context));
+		
+		Assert.assertEquals(
+				parse("A = B"), 
+				simplifier.apply(parse("A in {{B}}"), context));
+		
+		Assert.assertEquals(
+				parse("true"), 
+				simplifier.apply(parse("A in {A}"), context));
+		
+		Assert.assertEquals(
+				parse("true"), 
+				simplifier.apply(parse("A in {{A}}"), context));
+	}
+	
+	@Test
+	public void testElementInCardinalityGreaterThanOneSet() {
+		Assert.assertEquals(
+				parse("A = B or A in {C}"), 
+				simplifier.apply(parse("A in {B, C}"), context));
+		
+		Assert.assertEquals(
+				parse("A = B or A in {{C}}"), 
+				simplifier.apply(parse("A in {{B, C}}"), context));
+		
+		Assert.assertEquals(
+				parse("A = B or A in {C, D}"), 
+				simplifier.apply(parse("A in {B, C, D}"), context));
+		
+		Assert.assertEquals(
+				parse("A = B or A in {{C, D}}"), 
+				simplifier.apply(parse("A in {{B, C, D}}"), context));
+		
+		Assert.assertEquals(
+				parse("true"), 
+				simplifier.apply(parse("A in {A, B, C}"), context));
+		
+		Assert.assertEquals(
+				parse("true"), 
+				simplifier.apply(parse("A in {{A, B, C}}"), context));
+	}
+}
