d9da7b65dfc233b25080b8bf2d48a1fa6ff3bdac
Ciaran O'Reilly <oreilly@ai.sri.com>
2017-04-12 14:27:00 -0700

    Add union of sets equal empty set simplifier.

---
+++ src/test/java/com/sri/ai/test/grinder/sgdpllt/library/set/invsupport/UnionOfSetsEqualEmptySetSimplifierTest.java
@@ -0,0 +1,37 @@
+package com.sri.ai.test.grinder.sgdpllt.library.set.invsupport;
+
+import static com.sri.ai.expresso.helper.Expressions.parse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.sri.ai.grinder.sgdpllt.api.Context;
+import com.sri.ai.grinder.sgdpllt.core.TrueContext;
+import com.sri.ai.grinder.sgdpllt.library.set.invsupport.UnionOfSetsEqualEmptySetSimplifier;
+
+public class UnionOfSetsEqualEmptySetSimplifierTest {
+	private UnionOfSetsEqualEmptySetSimplifier simplifier = new UnionOfSetsEqualEmptySetSimplifier();
+	
+private Context context = new TrueContext();
+	
+	@Test
+	public void test1() {
+		Assert.assertEquals(
+				parse("{A} = {}"), 
+				simplifier.apply(parse("union({A}) = {}"), context));
+	}	
+
+	@Test
+	public void test2() {
+		Assert.assertEquals(
+				parse("{A} = {} and {B} = {}"), 
+				simplifier.apply(parse("{A} union {B} = {}"), context));
+	}	
+	
+	@Test
+	public void test3() {
+		Assert.assertEquals(
+				parse("{A} = {} and {B} = {} and {C} = {}"), 
+				simplifier.apply(parse("{A} union {B} union {C} = {}"), context));
+	}	
+}
