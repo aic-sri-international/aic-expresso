dd810f85ed31f85b34f72da0495c036ceb342787
Ciaran O'Reilly <oreilly@ai.sri.com>
2017-04-11 19:18:29 -0700

    Add element of intensional union simplifier.
    Along with supporting tests.

---
+++ src/test/java/com/sri/ai/test/grinder/sgdpllt/library/set/invsupport/ElementOfIntensionalUnionSimplifierTest.java
@@ -0,0 +1,26 @@
+package com.sri.ai.test.grinder.sgdpllt.library.set.invsupport;
+
+import static com.sri.ai.expresso.helper.Expressions.parse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.sri.ai.grinder.sgdpllt.api.Context;
+import com.sri.ai.grinder.sgdpllt.core.TrueContext;
+import com.sri.ai.grinder.sgdpllt.library.set.invsupport.ElementOfIntensionalUnionSimplifier;
+
+public class ElementOfIntensionalUnionSimplifierTest {
+	private ElementOfIntensionalUnionSimplifier simplifier = new ElementOfIntensionalUnionSimplifier();
+	private Context context = new TrueContext();
+	
+	@Test
+	public void testElementOfIntensionalUnion() {
+		Assert.assertEquals(
+				parse("there exists I in 1..10 : A in {I}"), 
+				simplifier.apply(parse("A in Union({{(on I in 1..10) {I} }})"), context));
+		
+		Assert.assertEquals(
+				parse("there exists I in 1..10 : I > 4 and A in {I}"), 
+				simplifier.apply(parse("A in Union({{(on I in 1..10) {I} : I > 4}})"), context));
+	}	
+}
