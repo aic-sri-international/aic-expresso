0bd3e1eb027f1c55293197d7e9e7b4d333e97b60
Charles Maussion <charles.maussion@gmail.com>
2017-05-30 10:17:55 -0700

    Skeleton component

--- src/main/java/com/sri/ai/grinder/sgdpllt/anytime/component.java
+++ src/main/java/com/sri/ai/grinder/sgdpllt/anytime/component.java
@@ -1,18 +1,35 @@
 package com.sri.ai.grinder.sgdpllt.anytime;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.sri.ai.expresso.api.Expression;
+import com.sri.ai.expresso.core.DefaultSymbol;
 
 public class component {
 
 	public Expression V;
-	public component[] children;
-	public Expression[] Dext;
-	public Expression[] D;
-	public Expression[] B;
-	public Expression[] Pint;
+	public Set<component> children;
+	public Set<Expression> Dext;
+	public Set<Expression> D;
+	public Set<Expression> B;
+	public Set<Expression> Pint;
 	
 	
-	public component(Expression[] Pext){
+	public component(Expression V, Expression Phi, Set<Expression> Pext){
+		this.V = V;
+		this.children = new HashSet<component>();
+		this.D = new HashSet<Expression>();
+		
+		this.Pint = new HashSet<Expression>();
+		this.Pint.add(Phi);
+
+		this.Dext = new HashSet<Expression>();
+		//Set<Expression> intersection = new HashSet<Expression>(Pext.getVariable); 
+		//intersection.retainAll(S);
+		
+		this.B = new HashSet<Expression>();
+		// this.B = Simplex(V)
 	}
 	
 	
