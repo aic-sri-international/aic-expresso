7e326de535916044893916db7accea38bc9416c8
Gabriel-Azevedo-Ferreira <gabriel.azevedo-ferreira@polytechnique.edu>
2017-06-12 23:21:24 -0700

    added tests, cleaned code, added some exlanation and examples for how to use the bound abstracting the type chosen

--- src/test/java/com/sri/ai/test/grinder/sgdpllt/library/bounds/BoundTest.java
+++ src/test/java/com/sri/ai/test/grinder/sgdpllt/library/bounds/BoundTest.java
@@ -18,16 +18,16 @@ import com.sri.ai.grinder.sgdpllt.core.TrueContext;
 import com.sri.ai.grinder.sgdpllt.library.bounds.Bound;
 import com.sri.ai.grinder.sgdpllt.library.bounds.Bounds;
 import com.sri.ai.grinder.sgdpllt.library.bounds.DefaultExtensionalBound;
-import com.sri.ai.grinder.sgdpllt.library.set.extensional.ExtensionalSets;
+import com.sri.ai.grinder.sgdpllt.library.bounds.DefaultIntensionalBound;
 import com.sri.ai.grinder.sgdpllt.theory.compound.CompoundTheory;
 import com.sri.ai.grinder.sgdpllt.theory.differencearithmetic.DifferenceArithmeticTheory;
 import com.sri.ai.grinder.sgdpllt.theory.equality.EqualityTheory;
 import com.sri.ai.grinder.sgdpllt.theory.linearrealarithmetic.LinearRealArithmeticTheory;
 import com.sri.ai.grinder.sgdpllt.theory.propositional.PropositionalTheory;
 import com.sri.ai.grinder.sgdpllt.theory.tuple.TupleTheory;
+
 import static com.sri.ai.util.Util.arrayList;
-
-
+import static com.sri.ai.expresso.helper.Expressions.makeSymbol;
 
 public class BoundTest {
 	
@@ -36,9 +36,38 @@ public class BoundTest {
 	Bound setOFNumbers;
 	Bound setOfFactors;
 	Bound extensionalBound;
+	Bound intensionalBound;
+	Bound IntensionalSetOfFactors1;
+	Bound IntensionalSetOfFactors2;
+	Bound IntensionalSetOfFactors3;
+	Model m;
 	
 	private void declaringTheoryContextAndSetOfFactors() {
-
+		
+		/*That's How we create a empty bound
+		 * It is useful for abstracting the kind of bound we are talking about:
+		 * 		if you say to a program that one of his attributes is a bound, you can let the user choose
+		 * 		between a extensional or intensional representation just by passing the right object as 
+		 * 		argument.
+		 * 
+		 * 		Example:
+		 * 		
+		 * 		class foo{
+		 * 			Bound b
+		 * 			...
+		 * 			public foo(Bound b, ...){
+		 *				this.b = b;
+		 *				...
+		 *			} 	 
+		 * 		}
+		 * 		
+		 * 		Use:
+		 * 		foo object = new foo(new DefaultExtensionalBound(),...);
+		 * 		
+		 * */
+		extensionalBound = new DefaultExtensionalBound();
+		intensionalBound = new DefaultIntensionalBound();
+		
 		theory = new CompoundTheory(
 								new EqualityTheory(false, true),
 								new DifferenceArithmeticTheory(false, false),
@@ -60,6 +89,10 @@ public class BoundTest {
 		Expression phi3 = parse("if X = true then 7 else if B = true then 8 else 9");
 		Expression phi4 = parse("if B = true then 10 else if A = true then 11 else 12");
 		Expression phi5 = parse("if C < 4 then 10 else if C = 4 then 11 else 12");
+		
+		/*
+		 * This is How we create a non empty Extensional bound
+		 * */
 		setOfFactors = new DefaultExtensionalBound(arrayList(phi1, phi2, phi3, phi4, phi5)); 
 	
 		//Set of numbers
@@ -67,64 +100,73 @@ public class BoundTest {
 		Expression two   = DefaultSymbol.createSymbol(2);
 		setOFNumbers = new DefaultExtensionalBound(arrayList(one,two));
 
-		extensionalBound = new DefaultExtensionalBound(); 
-	}
-	
-	@Test
-	public void testSimplex(){
-
-		Expression a = DefaultSymbol.createSymbol("A");
-		Expression b = DefaultSymbol.createSymbol("B");
-		Expression c = DefaultSymbol.createSymbol("C");
-		Expression d = DefaultSymbol.createSymbol("D");
-		//Expression q = DefaultSymbol.createSymbol("Q");
-
 		Set<Expression> Factor = new HashSet<Expression>();
 		
-		Model m = new Model(Factor);
+		m = new Model(Factor);
 		
 		m.context =	m.context.extendWithSymbolsAndTypes("A", "Boolean");
 		m.context =	m.context.extendWithSymbolsAndTypes("B", "Boolean");
 		m.context =	m.context.extendWithSymbolsAndTypes("Q", "Boolean");
 		m.context =	m.context.extendWithSymbolsAndTypes("C", "1..4");
-		m.context =	m.context.extendWithSymbolsAndTypes("D", "6..9");
+		m.context =	m.context.extendWithSymbolsAndTypes("D", "6..9");		
 		
-		//m.setType(a,  "Boolean");
-		//m.setType(b,  "Boolean");
-		//m.setType(q,  "Boolean");
+		IntensionalSetOfFactors1 = new DefaultIntensionalBound(
+				arrayList(
+						parse(" A' in Boolean"), 
+						parse("C' in 1..5")
+						),
+				parse("if C = C' then if A = A' then 1 else 4 else 0"),
+				makeSymbol(true)
+				);
 		
-		//Expression booleanExpression = parse("Boolean");
+		IntensionalSetOfFactors2 = intensionalBound.simplex(arrayList(parse("A")), m);
+		IntensionalSetOfFactors3 = intensionalBound.simplex(arrayList(parse("C"),parse("B")), m);
+	}
 	
-		//m.setValues(a,  booleanExpression);
-		//m.setValues(b,  booleanExpression);
-		//m.setValues(q,  booleanExpression);
+	@Test
+	public void testSimplex(){
+		declaringTheoryContextAndSetOfFactors();
 		
-		//VariableComponent ComponentResultat = new VariableComponent(q, null, m, new HashSet<Expression>());
+		Expression a = DefaultSymbol.createSymbol("A");
+		Expression b = DefaultSymbol.createSymbol("B");
+		Expression c = DefaultSymbol.createSymbol("C");
+		Expression d = DefaultSymbol.createSymbol("D");
 		
 		ArrayList<Expression> Variables = new ArrayList<>();
 		Variables.add(a);
 		Variables.add(b);
-		Variables.add(c);
 		Variables.add(d);
-
-		extensionalBound = new DefaultExtensionalBound(); 
-
 		
 		assertEquals(
-				parse("{ if A = true then 1 else 0, "
-					+ "if A = false then 1 else 0, "
-					+ "if B = true then 1 else 0, "
-					+ "if B = false then 1 else 0, "
-					+ "if C = 1 then 1 else 0, "
-					+ "if C = 2 then 1 else 0, "
-					+ "if C = 3 then 1 else 0, "
-					+ "if C = 4 then 1 else 0, "
-					+ "if D = 6 then 1 else 0, "
-					+ "if D = 7 then 1 else 0, "
-					+ "if D = 8 then 1 else 0, "
-					+ "if D = 9 then 1 else 0 }"),
+				parse("{ if A then if B then if D = 6 then 1 else 0 else 0 else 0, "
+						+ "if A then if B then if D = 7 then 1 else 0 else 0 else 0, "
+						+ "if A then if B then if D = 8 then 1 else 0 else 0 else 0, "
+						+ "if A then if B then if D = 9 then 1 else 0 else 0 else 0, "
+						+ "if A then if not B then if D = 6 then 1 else 0 else 0 else 0, "
+						+ "if A then if not B then if D = 7 then 1 else 0 else 0 else 0, "
+						+ "if A then if not B then if D = 8 then 1 else 0 else 0 else 0, "
+						+ "if A then if not B then if D = 9 then 1 else 0 else 0 else 0, "
+						+ "if not A then if B then if D = 6 then 1 else 0 else 0 else 0, "
+						+ "if not A then if B then if D = 7 then 1 else 0 else 0 else 0, "
+						+ "if not A then if B then if D = 8 then 1 else 0 else 0 else 0, "
+						+ "if not A then if B then if D = 9 then 1 else 0 else 0 else 0, "
+						+ "if not A then if not B then if D = 6 then 1 else 0 else 0 else 0, "
+						+ "if not A then if not B then if D = 7 then 1 else 0 else 0 else 0, "
+						+ "if not A then if not B then if D = 8 then 1 else 0 else 0 else 0, "
+						+ "if not A then if not B then if D = 9 then 1 else 0 else 0 else 0 }"),
 				extensionalBound.simplex(Variables, m));
 		
+		Variables.add(c);
+		
+		assertEquals(
+				parse("{ ( on A' in Boolean, B' in Boolean, D' in 6..9, C' in 1..4 ) "
+						+ "if C = C' then if D = D' then if B = B' then if A = A' then "
+						+ "1 else 0 else 0 else 0 else 0 }"),
+				intensionalBound.simplex(Variables, m));
+		
+		assertEquals(parse("{ ( on A' in Boolean) if A = A' then 1 else 0}"), IntensionalSetOfFactors2);
+		assertEquals(parse("{(on C' in 1..4, B' in Boolean) if B = B' then if C = C' then 1 else 0 else 0}"), IntensionalSetOfFactors3);
+		assertEquals(parse("{(on ) 1}"), intensionalBound.simplex(arrayList(), m));
 	}
 	
 	@Test
@@ -144,69 +186,43 @@ public class BoundTest {
 	public void testBoundProduct(){
 		declaringTheoryContextAndSetOfFactors();
 		
-		//Set of numbers
-		Expression one   = DefaultSymbol.createSymbol(1);
-		Expression two   = DefaultSymbol.createSymbol(2);
-		Expression setOFNumbers = ExtensionalSets.makeUniSet(one, two);
-
 		//Set of functions
-		Expression phi1 = parse("if C = 2 then 1 else 3");
-		Expression phi2 = parse("if A = true then  1 else 6");
-		Expression phi3 = parse("if X = true then 7 else if Y = true then 8 else 9");
-		Expression setOfFactors = ExtensionalSets.makeUniSet(phi1, phi2, phi3);
+		Expression phi = parse("if A = true then  1 else 6");
+		Bound setOfFactors2 = new DefaultExtensionalBound(phi);
 		
 		assertEquals(
-				 parse("{ if C = 2 then 1 else 9, "
-				 		+ "if C = 2 then 2 else 18, "
-				 		+ "if C = 2 then if A then 1 else 6 else if A then 3 else 18, "
-				 		+ "if C = 2 then if A then 2 else 12 else if A then 6 else 36, "
-				 		+ "if C = 2 then if X then 7 else if Y then 8 else 9 else if X then 21 else if Y then 24 else 27, "
-				 		+ "if C = 2 then if X then 14 else if Y then 16 else 18 else if X then 42 else if Y then 48 else 54, "
-				 		+ "if A then if C = 2 then 1 else 3 else if C = 2 then 6 else 18, "
-				 		+ "if A then if C = 2 then 2 else 6 else if C = 2 then 12 else 36, "
-				 		+ "if A then 1 else 36, if A then 2 else 72, "
-				 		+ "if A then if X then 7 else if Y then 8 else 9 else if X then 42 else if Y then 48 else 54, "
-				 		+ "if A then if X then 14 else if Y then 16 else 18 else if X then 84 else if Y then 96 else 108, "
-				 		+ "if X then if C = 2 then 7 else 21 else if Y then if C = 2 then 8 else 24 else if C = 2 then 9 else 27, "
-				 		+ "if X then if C = 2 then 14 else 42 else if Y then if C = 2 then 16 else 48 else if C = 2 then 18 else 54, "
-				 		+ "if X then if A then 7 else 42 else if Y then if A then 8 else 48 else if A then 9 else 54, "
-				 		+ "if X then if A then 14 else 84 else if Y then if A then 16 else 96 else if A then 18 else 108, "
-				 		+ "if X then 49 else if Y then 64 else 81, "
-				 		+ "if X then 98 else if Y then 128 else 162 }"
+				 parse( "{ if X then if A then 1 else 6 else if Y then if A then 2 else 12 else if A then 3 else 18, "
+				 		+ "if X then if A then 2 else 12 else if Y then if A then 4 else 24 else if A then 6 else 36, "
+				 		+ "if A then if Y then 4 else 5 else 36, if A then if Y then 8 else 10 else 72, "
+				 		+ "if X then if A then 7 else 42 else if B then if A then 8 else 48 else if A then 9 else 54, "
+				 		+ "if X then if A then 14 else 84 else if B then if A then 16 else 96 else if A then 18 else 108, "
+				 		+ "if B then if A then 10 else 60 else if A then 11 else 72, if B then if A then 20 else 120 else "
+				 		+ "if A then 22 else 144, if C < 4 then if A then 10 else 60 else if C = 4 then if A then 11 else 66 else if A then 12 else 72, "
+				 		+ "if C < 4 then if A then 20 else 120 else if C = 4 then if A then 22 else 132 else if A then 24 else 144 }"
 						),
-				 extensionalBound.boundProduct(theory, context, setOfFactors,setOfFactors,setOFNumbers));	 
+				 extensionalBound.boundProduct(theory, context,
+						 setOfFactors,
+						 setOfFactors2,
+						 setOFNumbers
+						 ));	 
 
-		assertEquals(parse("{1}"),Bounds.boundProduct(theory, context, new Expression[0]));
-		//Util.println(Bounds.boundProduct(theory, context, parse("{}"), parse("{1,2}")));
-	}
-	
-	@Test
-	public void testApplyFunctionToBound(){
-		
-		declaringTheoryContextAndSetOfFactors();
-
-		Expression phi = DefaultSymbol.createSymbol("phi");
-		Expression f = parse("13*phi");
+		assertEquals(
+				parse("{1}"),
+				Bounds.boundProduct(theory, context, new Expression[0]));
 		
 		assertEquals(
-				parse("{ 13, 26 }"),
-				Bounds.applyFunctionToBound(f, phi, setOFNumbers, theory, context)
-				);
+				parse("{ ( on A' in Boolean, C' in 1..5, C'' in 1..4, B' in Boolean ) if C = C' then if A then if A' then if B then if B' then if C' = C'' then 1 else 0 else 0 else if not B' then if C' = C'' then 1 else 0 else 0 else if B then if B' then if C' = C'' then 4 else 0 else 0 else if not B' then if C' = C'' then 4 else 0 else 0 else if not A' then if B then if B' then if C' = C'' then 1 else 0 else 0 else if not B' then if C' = C'' then 1 else 0 else 0 else if B then if B' then if C' = C'' then 4 else 0 else 0 else if not B' then if C' = C'' then 4 else 0 else 0 else 0 }"),
+				intensionalBound.boundProduct(theory, context, IntensionalSetOfFactors1, IntensionalSetOfFactors3));	
 		
 		assertEquals(
-				parse("{ if X then 13 else if Y then 26 else 39, "
-						+ "if A then if Y then 52 else 65 else 78, "
-						+ "if X then 91 else if B then 104 else 117, "
-						+ "if B then 130 else if A then 143 else 156, "
-						+ "if C < 4 then 130 else if C = 4 then 143 else 156 }"),
-				extensionalBound.applyFunctionToBound(f, phi, setOfFactors, theory, context)
-				); 
+				parse("{ ( on A' in Boolean ) if A then if A' then 1 else 0 else if not A' then 1 else 0 }"),
+				intensionalBound.boundProduct(theory, context, intensionalBound.simplex(arrayList(parse("A")), m)));
 	}
 	
 	@Test
 	public void testSummingOverBound(){
 		declaringTheoryContextAndSetOfFactors();
-		
+
 		assertEquals(
 			  "{ if Y then 3/7 else 4/7, "
 			+ "if Y then 10/21 else 11/21, "
