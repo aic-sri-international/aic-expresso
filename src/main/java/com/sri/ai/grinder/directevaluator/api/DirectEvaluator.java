33f96beadd03f9858664d0677feeaad87950aeb8
Bobak <Bobak@192.168.100.224>
2018-08-28 11:47:50 -0700

    - DirectEvaluator now extends Simplifier

--- src/main/java/com/sri/ai/grinder/directevaluator/api/DirectEvaluator.java
+++ src/main/java/com/sri/ai/grinder/directevaluator/api/DirectEvaluator.java
@@ -5,9 +5,20 @@ import java.io.InvalidClassException;
 import com.sri.ai.expresso.api.Expression;
 import com.sri.ai.expresso.helper.NormalizedExpression;
 import com.sri.ai.grinder.api.Context;
+import com.sri.ai.grinder.rewriter.api.Simplifier;
 
-public interface DirectEvaluator {
+public interface DirectEvaluator extends Simplifier {
 	
 	NormalizedExpression evaluate(Expression expression, Context context) throws InvalidClassException ;
+	
+	default Expression applySimplifier(Expression expression, Context context) {
+		Expression result = null;
+		try {
+			result = evaluate(expression, context);
+		} catch (InvalidClassException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 }
